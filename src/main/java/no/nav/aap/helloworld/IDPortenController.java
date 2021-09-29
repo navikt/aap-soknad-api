854148546144e807b0c2f604b36ef3878a60f612
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-09-29 09:36:26 +0200

    controller

--- src/main/java/no/nav/aap/helloworld/IDPortenController.java
+++ src/main/java/no/nav/aap/helloworld/IDPortenController.java
@@ -1,9 +1,16 @@
 package no.nav.aap.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class IDPortenController {
+
+
+    @GetMapping(path = "me")
+    public String me () {
+        return "hello";
+    }
 
 
 }
