fa28f5476f1a850aa40590fe1b36c3d62507f56b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-09-30 09:53:02 +0200

    annotate

--- src/main/java/no/nav/aap/helloworld/HelloWorldApplication.java
+++ src/main/java/no/nav/aap/helloworld/HelloWorldApplication.java
@@ -1,5 +1,6 @@
 package no.nav.aap.helloworld;
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import static no.nav.foreldrepenger.boot.conditionals.Cluster.profiler;
 
 @SpringBootApplication
+@EnableJwtTokenValidation
 public class HelloWorldApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(HelloWorldApplication.class)
