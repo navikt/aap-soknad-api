d6b6a7f2da30696f2df2ed74a1431f425e087194
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-09-30 14:10:37 +0200

    error handler

--- src/main/java/no/nav/aap/helloworld/AAPExceptionHandler.java
+++ src/main/java/no/nav/aap/helloworld/AAPExceptionHandler.java
@@ -4,6 +4,7 @@ import no.nav.security.token.support.core.exceptions.JwtTokenMissingException;
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
@@ -22,9 +27,20 @@ import java.net.URI;
 public class AAPExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(ExceptionHandler.class);
 
+    private final URI  wonderwall;
+
+    public AAPExceptionHandler(@Value("wonderwall.uri")URI wonderwall ) {
+        this.wonderwall = wonderwall;
+    }
+
     @ExceptionHandler
-    public ResponseEntity<Object> handleUncaught(Exception e, WebRequest req)  {
+    public ResponseEntity<Object> handleUncaught(Exception e, HttpServletRequest req)  {
         HttpHeaders headers = new HttpHeaders();
+        var  ny = UriComponentsBuilder.newInstance()
+                .scheme(wonderwall.getScheme())
+                .host(wonderwall.getHost())
+                .path("/oauth2/login").queryParam("redirect",req.getRequestURL()).build().toUri();
+        LOG.info("XXXX " + ny);
         headers.setLocation(URI.create("http://www.vg.no"));
         return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
     }
