155c756f978e70712a9156878eafa798299ce718
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-09-30 11:01:51 +0200

    no proxy

--- src/main/java/no/nav/aap/helloworld/IDPortenController.java
+++ src/main/java/no/nav/aap/helloworld/IDPortenController.java
@@ -4,7 +4,7 @@ import no.nav.security.token.support.spring.ProtectedRestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@ProtectedRestController(value = "/api", issuer = "test")
+@ProtectedRestController(value = "/api", issuer = "idporten")
 public class IDPortenController {
 
 
