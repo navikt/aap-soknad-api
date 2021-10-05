c09178d18f8167d5b2c88a0da865f39534f2a319
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-05 18:36:38 +0200

    pdl

--- src/main/java/no/nav/aap/api/AAPApiApplication.java
+++ src/main/java/no/nav/aap/api/AAPApiApplication.java
@@ -4,12 +4,14 @@ import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import static no.nav.foreldrepenger.boot.conditionals.Cluster.profiler;
 
 @SpringBootApplication
 @EnableJwtTokenValidation
 @EnableOAuth2Client(cacheEnabled = true)
+@ConfigurationPropertiesScan
 public class AAPApiApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(AAPApiApplication.class)
