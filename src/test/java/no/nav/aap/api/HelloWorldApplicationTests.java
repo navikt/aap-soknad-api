36b714a648e45a32ba77a5736a00f623bedc7aea
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-05 13:00:42 +0200

    pdl

--- src/test/java/no/nav/aap/api/HelloWorldApplicationTests.java
+++ src/test/java/no/nav/aap/api/HelloWorldApplicationTests.java
@@ -3,7 +3,7 @@ package no.nav.aap.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HelloWorldApplicationTests {
 
     @Test
