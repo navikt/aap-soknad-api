d33d0e8837385fcfc8cfb28e7f0d651069f3f05e
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-07 11:51:29 +0200

    property order

--- src/main/java/no/nav/aap/api/domain/Fødselsnummer.java
+++ src/main/java/no/nav/aap/api/domain/Fødselsnummer.java
@@ -1,10 +1,8 @@
 package no.nav.aap.api.domain;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 import static no.nav.aap.api.util.StringUtil.partialMask;
 
-public record Fødselsnummer(@JsonValue String fnr) {
+public record Fødselsnummer(String fnr) {
 
     @Override
     public String toString() {
