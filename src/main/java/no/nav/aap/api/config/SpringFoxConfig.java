a2187ce98b184bd18df1566c034b659c708c5814
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-07 10:16:32 +0200

    codeowners

--- src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
+++ src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
@@ -1,12 +1,12 @@
 package no.nav.aap.api.config;
 
-import io.swagger.models.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
