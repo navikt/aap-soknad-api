0a61fdff2df3b1e52d43eb2bcff54aac6adc6aff
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-07 09:28:07 +0200

    swagger

--- src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
+++ src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
@@ -9,14 +9,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Set;
 
-import static springfox.documentation.spi.DocumentationType.OAS_30;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
-        return new Docket(OAS_30)
+        return new Docket(SWAGGER_2)
                 .protocols(Set.of("http", "https"))
                 .select()
                 .apis(RequestHandlerSelectors.any())
