ba1614704f90f653aba092b2177a5dca3484e035
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-07 10:25:42 +0200

    codeowners

--- src/main/java/no/nav/aap/api/APIController.java
+++ src/main/java/no/nav/aap/api/APIController.java
@@ -1,14 +1,15 @@
 package no.nav.aap.api;
 
-import no.nav.aap.api.config.AbstractRestConfig;
 import no.nav.aap.api.pdl.PDLService;
 import no.nav.aap.api.util.TokenUtil;
 import no.nav.security.token.support.core.api.Unprotected;
 import no.nav.security.token.support.spring.ProtectedRestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static no.nav.aap.api.config.AbstractRestConfig.ISSUER;
 
-@ProtectedRestController(value = "/api", issuer = AbstractRestConfig.ISSUER)
+
+@ProtectedRestController(value = "/api", issuer = ISSUER)
 public class APIController {
 
     private final TokenUtil tokenUtil;
--- src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
+++ src/main/java/no/nav/aap/api/config/SpringFoxConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -12,6 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import java.util.List;
 import java.util.Set;
 
+import static springfox.documentation.builders.PathSelectors.any;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.OAS_30;
 
@@ -21,10 +21,11 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(OAS_30)
+                .apiInfo(apiInfo())
                 .protocols(Set.of("http", "https"))
                 .select()
                 .apis(basePackage("no.nav.aap"))
-                .paths(PathSelectors.any())
+                .paths(any())
                 .build();
     }
     private ApiInfo apiInfo() {
@@ -32,7 +33,7 @@ public class SpringFoxConfig {
                 "AAP Søknad api",
                 "API for å ta i mot søknader",
                 "1.0",
-                "Terms of service",
+                null,
                 new Contact("Jan-Olav Eide", "www.nav.no", "jan-olav.eide@nav.no"),
                 "MIT",
                 "https://github.com/navikt/aap-soeknad-api/blob/main/LICENSE.md",
