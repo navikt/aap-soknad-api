a921dfde65bf2c6bf0a855195906ee8a32bb9035
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-07 11:37:17 +0200

    recordify

--- src/main/java/no/nav/aap/api/domain/Fødselsnummer.java
+++ src/main/java/no/nav/aap/api/domain/Fødselsnummer.java
@@ -3,17 +3,10 @@ package no.nav.aap.api.domain;
 import com.fasterxml.jackson.annotation.JsonValue;
 import static no.nav.aap.api.util.StringUtil.*;
 
-public class Fødselsnummer {
+public record Fødselsnummer(@JsonValue String fnr) {
 
-        @JsonValue
-        private final String fnr;
-
-        public Fødselsnummer(String fnr) {
-            this.fnr = fnr;
-        }
-
-        @Override
-        public String toString() {
-            return getClass().getSimpleName() + " [fnr=" + partialMask(fnr) + "]";
-        }
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [fnr=" + partialMask(fnr) + "]";
     }
+}
