b7f353a37ee8272c3e2d68546d5ee3551f548305
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-08 09:57:21 +0200

    health

--- src/main/java/no/nav/aap/api/pdl/PDLHealthIndicator.java
+++ src/main/java/no/nav/aap/api/pdl/PDLHealthIndicator.java
@@ -3,11 +3,9 @@ package no.nav.aap.api.pdl;
 import no.nav.aap.api.health.AbstractPingableHealthIndicator;
 import org.springframework.stereotype.Component;
 
-public class PDLHealthIndicator {
     @Component
-    public class OppslagHealthIndicator extends AbstractPingableHealthIndicator {
-        public OppslagHealthIndicator(PDLConnection connection) {
+    public class PDLHealthIndicator extends AbstractPingableHealthIndicator {
+        public PDLHealthIndicator(PDLConnection connection) {
             super(connection);
         }
-    }
 }
