75f0055b3bb5c9203f8221b7920e878edf262f9a
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-09 10:47:08 +0200

    info

--- src/main/java/no/nav/aap/api/søknad/domain/UtenlandsSøknad.java
+++ src/main/java/no/nav/aap/api/søknad/domain/UtenlandsSøknad.java
@@ -1,4 +1,6 @@
 package no.nav.aap.api.søknad.domain;
 
-public class UtenlandsSøknad {
+import com.neovisionaries.i18n.CountryCode;
+
+public record UtenlandsSøknad(CountryCode land, Periode periode) {
 }
