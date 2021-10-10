1224260768059ed47298316c30b2172fbd456630
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-10 09:41:26 +0200

    unwrap fix pre jackson 2.14

--- src/main/java/no/nav/aap/api/søknad/domain/Fødselsnummer.java
+++ src/main/java/no/nav/aap/api/søknad/domain/Fødselsnummer.java
@@ -1,11 +1,18 @@
 package no.nav.aap.api.søknad.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import javax.validation.constraints.NotNull;
 
 import static no.nav.aap.api.søknad.util.StringUtil.partialMask;
 
-public record Fødselsnummer(@NotNull String fnr) {
+public record Fødselsnummer(@NotNull @JsonValue String fnr) {
 
+    @JsonValue(false)
+    @Override
+    public String fnr() {
+        return fnr;
+    }
     @Override
     public String toString() {
         return getClass().getSimpleName() + " [fnr=" + partialMask(fnr) + "]";
--- src/main/java/no/nav/aap/api/søknad/domain/Søker.java
+++ src/main/java/no/nav/aap/api/søknad/domain/Søker.java
@@ -1,4 +1,3 @@
 package no.nav.aap.api.søknad.domain;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-public record Søker(@JsonUnwrapped Fødselsnummer fnr,Navn navn) {
+public record Søker( Fødselsnummer fnr,Navn navn) {
 }
