1157223c2e4d550cf3830f1b12dc4185de13e6ae
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-15 13:04:37 +0200

    nullale

--- src/main/kotlin/no/nav/aap/api/søknad/domain/Navn.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/domain/Navn.kt
@@ -3,4 +3,4 @@ package no.nav.aap.api.søknad.domain
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
 @JsonPropertyOrder("fornavn", "mellomnavn", "etternavn")
-data class Navn(val fornavn: String,val mellomnavn: String,val etternavn: String)
+data class Navn(val fornavn: String?,val mellomnavn: String?,val etternavn: String?)
