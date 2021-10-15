3eb187354753596017e2eb268b197c4060e46087
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-15 10:58:58 +0200

    lambda

--- src/main/kotlin/no/nav/aap/api/søknad/tokenx/TokenXConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/tokenx/TokenXConfig.kt
@@ -13,8 +13,8 @@ import java.net.URI
 @Configuration
 class TokenXConfig {
     @Bean
-    fun configMatcher(): (ClientConfigurationProperties, URI) -> ClientProperties? {
-        return {  cfg: ClientConfigurationProperties, req: URI ->  cfg.registration[req.host.split("\\.".toRegex()).toTypedArray()[0]]  }
+    fun configMatcher(): TokenXConfigMatcher {
+        return CCM()
     }
     @Bean
     fun customizer(): Jackson2ObjectMapperBuilderCustomizer {
@@ -27,4 +27,10 @@ class TokenXConfig {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     private interface IgnoreUnknownMixin
+
+    internal class CCM : TokenXConfigMatcher {
+        override fun findProperties(cfg: ClientConfigurationProperties, uri: URI): ClientProperties? {
+            return cfg.registration[uri.host.split("\\.".toRegex()).toTypedArray()[0]]
+        }
+    }
 }
