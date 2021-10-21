bfdd9ef8f68e6e5aa330df98257af7e549944efa
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-21 12:01:28 +0200

    package

--- src/main/kotlin/no/nav/aap/api/oppslag/Navn.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.aap.api.oppslag
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
-
-@JsonPropertyOrder("fornavn", "mellomnavn", "etternavn")
-data class Navn(val fornavn: String?,val mellomnavn: String?,val etternavn: String?)
