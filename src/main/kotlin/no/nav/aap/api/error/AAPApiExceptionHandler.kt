7597c15ea56eb24ebf7b0733e03360a3597fcfb9
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-21 15:14:26 +0200

    ekspertimenter med problem handling

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -1,12 +1,10 @@
 package no.nav.aap.api.error
 
 import no.nav.aap.api.util.AuthContext
-import no.nav.foreldrepenger.boot.conditionals.EnvUtil
-import no.nav.foreldrepenger.boot.conditionals.EnvUtil.*
+import no.nav.foreldrepenger.boot.conditionals.EnvUtil.isDevOrLocal
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.slf4j.LoggerFactory
-import org.springframework.context.EnvironmentAware
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -17,7 +15,7 @@ import org.zalando.problem.spring.webflux.advice.ProblemHandling
 
 
 @ControllerAdvice
-class AAPApiExceptionHandler(val authContext: AuthContext, private val env: Environment) :EnvironmentAware, ProblemHandling {
+class AAPApiExceptionHandler(val authContext: AuthContext, private val env: Environment) : ProblemHandling {
 
     private val LOG = LoggerFactory.getLogger(AAPApiExceptionHandler::class.java)
 
