5f603e664f7f44da297bbcc160e54b55c16d32bb
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-21 12:25:57 +0200

    ekspertimenter med problem handling

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -3,33 +3,20 @@ package no.nav.aap.api.error
 import no.nav.aap.api.util.AuthContext
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.UNAUTHORIZED
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.zalando.problem.Problem
+import org.zalando.problem.Status
+import org.zalando.problem.ThrowableProblem
+import org.zalando.problem.spring.web.advice.ProblemHandling
 
 
 @ControllerAdvice
-class AAPApiExceptionHandler(val authContext: AuthContext) : ResponseEntityExceptionHandler() {
+class AAPApiExceptionHandler(val authContext: AuthContext) :  ProblemHandling {
     @ExceptionHandler(JwtTokenUnauthorizedException::class, JwtTokenMissingException::class)
-    fun handleMissingOrExpiredToken(e: java.lang.Exception, req: WebRequest): ResponseEntity<Any> {
-         return handle(UNAUTHORIZED,e,req,HttpHeaders());
-    }
-
-    private  fun handle(status: HttpStatus, e: java.lang.Exception, req: WebRequest, vararg messages: Any): ResponseEntity<Any> {
-        return handle(status, e, req, HttpHeaders(), *messages)
-    }
-
-    private  fun handle(status: HttpStatus, e: java.lang.Exception, req: WebRequest, headers: HttpHeaders, vararg messages: Any): ResponseEntity<Any> {
-        return handle(status, e, req, headers, listOf(messages))
-    }
-
-    private  fun handle(status: HttpStatus, e: java.lang.Exception, req: WebRequest, headers: HttpHeaders, messages: List<Any>): ResponseEntity<Any> {
-        return handleExceptionInternal(e, e.message, headers, status, req)
+    fun handleMissingOrExpiredToken(e: java.lang.Exception, req: WebRequest): ThrowableProblem? {
+        return Problem.valueOf(Status.UNAUTHORIZED);
     }
 
     override fun toString() = "${javaClass.simpleName} [authContext=$authContext]"
