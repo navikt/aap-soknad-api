88deebb8c33cedaf5dc0579fe3def4ce4786304b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-21 15:07:49 +0200

    ekspertimenter med problem handling

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -1,9 +1,13 @@
 package no.nav.aap.api.error
 
 import no.nav.aap.api.util.AuthContext
+import no.nav.foreldrepenger.boot.conditionals.EnvUtil
+import no.nav.foreldrepenger.boot.conditionals.EnvUtil.*
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.slf4j.LoggerFactory
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -13,15 +17,19 @@ import org.zalando.problem.spring.webflux.advice.ProblemHandling
 
 
 @ControllerAdvice
-class AAPApiExceptionHandler(val authContext: AuthContext) : ProblemHandling {
+class AAPApiExceptionHandler(val authContext: AuthContext, private val env: Environment) :EnvironmentAware, ProblemHandling {
 
     private val LOG = LoggerFactory.getLogger(AAPApiExceptionHandler::class.java)
 
     @ExceptionHandler(JwtTokenUnauthorizedException::class, JwtTokenMissingException::class)
     fun handleMissingOrExpiredToken(e: java.lang.Exception, req: WebRequest) {
-         LOG.warn("OOPS",e)
          throw Problem.valueOf(Status.UNAUTHORIZED);
     }
+
+    override fun isCausalChainsEnabled(): Boolean {
+        return isDevOrLocal(env);
+    }
+
 
     override fun toString() = "${javaClass.simpleName} [authContext=$authContext]"
 }
