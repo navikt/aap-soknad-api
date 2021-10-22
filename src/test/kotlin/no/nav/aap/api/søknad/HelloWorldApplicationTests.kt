d8abfa892a3575eeab8e87cbb48f394e088364a7
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-22 10:22:22 +0200

    no test yet

--- src/test/kotlin/no/nav/aap/api/søknad/HelloWorldApplicationTests.kt
+++ src/test/kotlin/no/nav/aap/api/søknad/HelloWorldApplicationTests.kt
@@ -1,9 +1,8 @@
 package no.nav.aap.api.søknad
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 internal class  HelloWorldApplicationTests {
     @Test
     fun contextLoads() {
