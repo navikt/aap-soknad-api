3a3799cf4660ce8af41e5c7c1a26a7ff2c463211
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-10-27 14:34:26 +0200

    no flux, try 1

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -5,20 +5,21 @@ import no.nav.boot.conditionals.EnvUtil.isDevOrLocal
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.springframework.core.env.Environment
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.context.request.WebRequest
+import org.springframework.web.context.request.NativeWebRequest
 import org.zalando.problem.Problem
-import org.zalando.problem.Status.UNAUTHORIZED
-import org.zalando.problem.spring.webflux.advice.ProblemHandling
+import org.zalando.problem.Status
+import org.zalando.problem.spring.web.advice.ProblemHandling
 
 
 @ControllerAdvice
 class AAPApiExceptionHandler(val authContext: AuthContext, private val env: Environment) : ProblemHandling {
 
     @ExceptionHandler(JwtTokenUnauthorizedException::class, JwtTokenMissingException::class)
-    fun handleMissingOrExpiredToken(e: java.lang.Exception, req: WebRequest) {
-         throw Problem.valueOf(UNAUTHORIZED)
+    fun handleMissingOrExpiredToken(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem>? {
+        return create(Status.UNAUTHORIZED,e,req)
     }
 
     override fun isCausalChainsEnabled(): Boolean {
