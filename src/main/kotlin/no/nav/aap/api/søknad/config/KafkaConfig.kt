bb8bcff06b62e85604e7226b11c4201b8a485566
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-01 12:43:41 +0100

    autoconf via properties

--- src/main/kotlin/no/nav/aap/api/søknad/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/config/KafkaConfig.kt
@@ -1,12 +1,10 @@
 package no.nav.aap.api.søknad.config
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 
-@ConfigurationProperties("kafka")
-@ConditionalOnProperty("kafka.brokers")
+//@ConfigurationProperties("kafka")
+//@ConditionalOnProperty("kafka.brokers")
 data class KafkaConfig @ConstructorBinding constructor (val brokers: String,
                                                    @NestedConfigurationProperty val truststorePath: TrustStore,
                                                    @NestedConfigurationProperty val credstorePassword: CredStore,
@@ -14,5 +12,3 @@ data class KafkaConfig @ConstructorBinding constructor (val brokers: String,
 data class TrustStore(val path: String)
 data class KeyStore(val path: String)
 data class CredStore(val password: String)
-
-
--- src/test/kotlin/no/nav/aap/api/søknad/AAPAPIApplicationTests.kt
+++ src/test/kotlin/no/nav/aap/api/søknad/AAPAPIApplicationTests.kt
@@ -3,15 +3,28 @@ package no.nav.aap.api.søknad
 import no.nav.aap.api.oppslag.pdl.PDLWebClientAdapter
 import no.nav.aap.api.rest.tokenx.TokenXFilterFunction
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ActiveProfiles
+
 
 @SpringBootTest
 @EnableMockOAuth2Server
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = arrayOf("listeners=PLAINTEXT://localhost:9092","port=9092"))
 internal class  AAPAPIApplicationTests {
+
+    @Autowired
+    private val consumer: KafkaConsumer<*, *>? = null
+
+    @Autowired
+    private val producer: KafkaProducer<*, *>? = null
+
 
     @MockBean
     private lateinit var pdl: PDLWebClientAdapter
