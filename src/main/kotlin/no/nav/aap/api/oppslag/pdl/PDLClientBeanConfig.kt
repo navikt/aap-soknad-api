726a5f1edc1b2b11d0c152d7cecdb7577d910b99
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-03 13:45:08 +0100

    log webclient

--- src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLClientBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLClientBeanConfig.kt
@@ -6,17 +6,22 @@ import no.nav.aap.api.config.Constants.PDL_USER
 import no.nav.aap.api.rest.AbstractRestConfig.Companion.correlatingFilterFunction
 import no.nav.aap.api.rest.AbstractRestConfig.Companion.temaFilterFunction
 import no.nav.aap.api.rest.tokenx.TokenXFilterFunction
+import no.nav.boot.conditionals.EnvUtil.isDevOrLocal
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
 
 @Configuration
  class PDLClientBeanConfig()  {
     @Qualifier(PDL_USER)
     @Bean
-     fun webClientPDL(builder: WebClient.Builder, cfg: PDLConfig, tokenXFilterFunction: TokenXFilterFunction): WebClient {
+     fun webClientPDL(builder: WebClient.Builder, cfg: PDLConfig, tokenXFilterFunction: TokenXFilterFunction, env: Environment): WebClient {
         return builder
+            .clientConnector(ReactorClientHttpConnector(HttpClient.create().wiretap(isDevOrLocal(env))))
             .baseUrl(cfg.baseUri.toString())
             .filter(correlatingFilterFunction())
             .filter(temaFilterFunction())
