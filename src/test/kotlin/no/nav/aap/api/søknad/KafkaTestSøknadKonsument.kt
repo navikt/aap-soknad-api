dd495036927f2005f550dd3803ba31eb347c548e
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-04 12:31:51 +0100

    trenger ikke annoteringene

--- src/test/kotlin/no/nav/aap/api/søknad/KafkaTestSøknadKonsument.kt
+++ src/test/kotlin/no/nav/aap/api/søknad/KafkaTestSøknadKonsument.kt
@@ -5,11 +5,8 @@ import no.nav.aap.api.søknad.model.UtenlandsSøknadKafka
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.stereotype.Component
 import java.util.concurrent.CountDownLatch
 
-@Profile(TEST)
-@Component
 class KafkaTestSøknadKonsument {
     var value: UtenlandsSøknadKafka? = null
     val latch = CountDownLatch(1)
