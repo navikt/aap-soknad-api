6ba79f1d7ddeb18e81fefe26a7e7b1679c63304f
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-25 21:16:13 +0100

    oneliner

--- src/main/kotlin/no/nav/aap/api/util/CallIdGenerator.kt
+++ src/main/kotlin/no/nav/aap/api/util/CallIdGenerator.kt
@@ -1,10 +1,8 @@
 package no.nav.aap.api.util
 
-import org.springframework.stereotype.Component
 import java.util.*
 
 object CallIdGenerator {
-    fun create(): String {
-        return UUID.randomUUID().toString()
-    }
+    fun create() =  UUID.randomUUID().toString()
+
 }
