93c69bfb7dd86eeb01595d2956007d6863e713ab
Richard Borge <richard.borge@nav.no>
2021-11-26 17:53:18 +0100

    Expose prometheus

--- src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
@@ -10,6 +10,10 @@ class SøknadMetrics(private val meterRegistry: MeterRegistry) {
 
     private val log = LoggerUtil.getLogger(javaClass)
 
+    private val TAG_TYPE = "type"
+    private val TAG_LAND = "land"
+    private val TAG_VARIGHET = "varighet"
+
     private val SØKNAD_UTLAND = "utland"
 
     private val COUNTER_SØKNAD_MOTTATT = "aap_soknad_mottatt"
@@ -18,8 +22,11 @@ class SøknadMetrics(private val meterRegistry: MeterRegistry) {
         runCatching {
             meterRegistry.counter(
                 COUNTER_SØKNAD_MOTTATT,
+                TAG_TYPE,
                 SØKNAD_UTLAND,
+                TAG_LAND,
                 land,
+                TAG_VARIGHET,
                 periode.varighetDager().toString()
             ).increment().also {
                 log.debug("incrementSøknadUtlandMottatt for $land")
