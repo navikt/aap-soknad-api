a560e303c9683a5053a602c8dcd29b5492413d08
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-30 19:14:01 +0100

    simplify main

--- src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
+++ src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
@@ -4,8 +4,8 @@ import no.nav.boot.conditionals.Cluster.profiler
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
 
@@ -17,13 +17,9 @@ import org.springframework.retry.annotation.EnableRetry
 @EnableRetry
 @EnableKafka
 class AAPSøknadApiApplication {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplicationBuilder(AAPSøknadApiApplication::class.java)
-                .profiles(*profiler())
-                .main(AAPSøknadApiApplication::class.java)
-                .run(*args)
+    fun main(args: Array<String>) {
+        runApplication<AAPSøknadApiApplication>(*args) {
+            setAdditionalProfiles(*profiler())
         }
     }
 }
