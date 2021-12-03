4a909e514488d35c4b974e63efa3b38730c5e33b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-03 15:06:00 +0100

    404 ved not found

--- src/main/kotlin/no/nav/aap/api/mellomlagring/GCPMellomlagring.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/GCPMellomlagring.kt
@@ -3,7 +3,6 @@ package no.nav.aap.api.mellomlagring
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Storage
-import com.google.cloud.storage.StorageException
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.søknad.SkjemaType
 import no.nav.aap.util.LoggerUtil.getLogger
@@ -13,8 +12,8 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.web.client.HttpClientErrorException
-import java.nio.charset.Charset
+import org.springframework.web.client.HttpClientErrorException.create
+import java.nio.charset.Charset.defaultCharset
 import java.nio.charset.StandardCharsets.UTF_8
 
 
@@ -31,20 +30,14 @@ class GCPMellomlagring(@Value("\${mellomlagring.bucket:aap-mellomlagring}") val 
 
 
     override fun les(fnr: Fødselsnummer, type: SkjemaType): String? {
-        try {
-            return storage.get(bøttenavn, key(fnr, type))?.getContent()?.let { String(it) }
-                ?: throw exception(NOT_FOUND, "Ingen mellomlagring funnet")
-        } catch (e: StorageException) {
-            log.warn("Uventet feil ved oppslag av mellomlagret  skjema for {},", fnr, e)
-            throw e
-        }
+        return storage.get(bøttenavn, key(fnr, type))?.getContent()?.let { String(it) }
+            ?: throw exception(NOT_FOUND, "Ingen mellomlagring funnet")
     }
 
     private fun exception(status: HttpStatus, msg: String) =
-        HttpClientErrorException.create(status, msg, HttpHeaders(), ByteArray(0), Charset.defaultCharset())
+        create(status, msg, HttpHeaders(), ByteArray(0), defaultCharset())
 
     override fun slett(fnr: Fødselsnummer, type: SkjemaType) = storage.delete(blobFra(fnr, type))
     private fun key(fnr: Fødselsnummer, type: SkjemaType) = type.name + "_" + fnr.fnr
     private fun blobFra(fnr: Fødselsnummer, type: SkjemaType) = BlobId.of(bøttenavn, key(fnr, type))
-
 }
