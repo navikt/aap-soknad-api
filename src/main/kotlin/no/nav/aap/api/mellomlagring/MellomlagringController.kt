becae06e1965f9494442723b0beda3d0fcc5e4b5
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-03 15:11:59 +0100

    return created

--- src/main/kotlin/no/nav/aap/api/mellomlagring/MellomlagringController.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/MellomlagringController.kt
@@ -4,6 +4,7 @@ import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.søknad.SkjemaType
 import no.nav.boot.conditionals.ConditionalOnDevOrLocal
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
@@ -19,11 +20,16 @@ import org.springframework.web.bind.annotation.RestController
 @Unprotected
 @RequestMapping(value = ["buckets"], produces = [APPLICATION_JSON_VALUE])
 @ConditionalOnDevOrLocal
-class GCPController(private val gcp: GCPMellomlagring) {
+class MellomlagringController(private val gcp: GCPMellomlagring) {
 
     @PostMapping("/lagre/{fnr}/{type}")
-    fun lagre(@PathVariable fnr: Fødselsnummer, @PathVariable type: SkjemaType, @RequestBody data: String) =
+    fun lagre(@PathVariable fnr: Fødselsnummer,
+              @PathVariable type: SkjemaType,
+              @RequestBody data: String): ResponseEntity<String> {
         gcp.lagre(fnr, type, data)
+        return ResponseEntity<String>(data, CREATED)
+    }
+
 
     @GetMapping("/les/{fnr}/{type}")
     fun les(@PathVariable fnr: Fødselsnummer, @PathVariable type: SkjemaType) =
