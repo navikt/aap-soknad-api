2862afea9c1c5a3cdd839446097136fe6c0a3b93
Richard Borge <richard.borge@nav.no>
2021-12-03 12:54:31 +0100

    Endrer navn på counter for enklere queries

--- src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
@@ -10,20 +10,15 @@ class SøknadMetrics(private val meterRegistry: MeterRegistry) {
 
     private val log = LoggerUtil.getLogger(javaClass)
 
-    private val TAG_TYPE = "type"
     private val TAG_LAND = "land"
     private val TAG_VARIGHET = "varighet"
 
-    private val SØKNAD_UTLAND = "utland"
-
-    private val COUNTER_SØKNAD_MOTTATT = "aap_soknad_mottatt"
+    private val COUNTER_SØKNAD_UTLAND_MOTTATT = "aap_soknad_utland_mottatt"
 
     fun incrementSøknadUtlandMottatt(land: String, periode: Periode) {
         runCatching {
             meterRegistry.counter(
-                    COUNTER_SØKNAD_MOTTATT,
-                    TAG_TYPE,
-                    SØKNAD_UTLAND,
+                    COUNTER_SØKNAD_UTLAND_MOTTATT,
                     TAG_LAND,
                     land,
                     TAG_VARIGHET,
