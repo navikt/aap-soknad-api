bd7ba01eb776fb16cd645dbdee1862ddd4758974
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-06 21:36:19 +0100

    chore

--- src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/SøknadMetrics.kt
@@ -10,10 +10,9 @@ class SøknadMetrics(private val meterRegistry: MeterRegistry) {
     private val TAG_VARIGHET = "varighet"
     private val COUNTER_SØKNAD_UTLAND_MOTTATT = "aap_soknad_utland_mottatt"
 
-    fun increment(søknad: UtenlandsSøknadKafka) {
+    fun increment(søknad: UtenlandsSøknadKafka) =
         meterRegistry.counter(COUNTER_SØKNAD_UTLAND_MOTTATT,
                 TAG_LAND, søknad.land.alpha3,
                 TAG_VARIGHET, søknad.periode.varighetDager().toString())
             .increment()
-    }
 }
