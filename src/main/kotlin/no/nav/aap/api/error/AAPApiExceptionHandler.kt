11fedf72a688983a026329f521efcf62350305b5
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-07 10:26:12 +0100

    StorageException -> BAD_REQUEST

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.api.error
 
+import com.google.cloud.storage.StorageException
 import no.nav.aap.util.AuthContext
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.context.request.NativeWebRequest
 import org.zalando.problem.Problem
+import org.zalando.problem.Status.BAD_REQUEST
 import org.zalando.problem.Status.NOT_FOUND
 import org.zalando.problem.Status.UNAUTHORIZED
 import org.zalando.problem.Status.UNPROCESSABLE_ENTITY
@@ -25,6 +27,10 @@ class AAPApiExceptionHandler(val authContext: AuthContext) : ProblemHandling {
     @ExceptionHandler(IntegrationException::class)
     fun handleIntegrationException(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
         create(UNPROCESSABLE_ENTITY, e, req)
+
+    @ExceptionHandler(StorageException::class)
+    fun handleStorageException(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
+        create(BAD_REQUEST, e, req)
 
     @ExceptionHandler(HttpClientErrorException.NotFound::class)
     fun handleNotFound(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
