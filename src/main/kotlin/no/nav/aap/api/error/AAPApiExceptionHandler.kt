76c9cbb3761838210592a07945338acce4a8f3de
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-07 19:38:59 +0100

    cleanup

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -2,7 +2,6 @@ package no.nav.aap.api.error
 
 import com.google.cloud.storage.StorageException
 import no.nav.aap.api.felles.error.IntegrationException
-import no.nav.aap.util.AuthContext
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.springframework.http.ResponseEntity
@@ -19,23 +18,21 @@ import org.zalando.problem.spring.web.advice.ProblemHandling
 
 
 @ControllerAdvice
-class AAPApiExceptionHandler(val authContext: AuthContext) : ProblemHandling {
+class AAPApiExceptionHandler : ProblemHandling {
 
     @ExceptionHandler(JwtTokenUnauthorizedException::class, JwtTokenMissingException::class)
     fun handleMissingOrExpiredToken(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
         create(UNAUTHORIZED, e, req)
 
     @ExceptionHandler(IntegrationException::class)
-    fun handleIntegrationException(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
+    fun handleIntegrationException(e: IntegrationException, req: NativeWebRequest): ResponseEntity<Problem> =
         create(UNPROCESSABLE_ENTITY, e, req)
 
     @ExceptionHandler(StorageException::class)
-    fun handleStorageException(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
+    fun handleStorageException(e: StorageException, req: NativeWebRequest): ResponseEntity<Problem> =
         create(BAD_REQUEST, e, req)
 
     @ExceptionHandler(HttpClientErrorException.NotFound::class)
-    fun handleNotFound(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
+    fun handleNotFound(e: HttpClientErrorException, req: NativeWebRequest): ResponseEntity<Problem> =
         create(NOT_FOUND, e, req)
-
-    override fun toString() = "${javaClass.simpleName} [authContext=$authContext]"
 }
