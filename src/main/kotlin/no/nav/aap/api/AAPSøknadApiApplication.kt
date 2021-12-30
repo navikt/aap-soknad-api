330e995bb28892e9b63506d95548e1deda0452be
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-30 20:10:59 +0100

    revert

--- src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
+++ src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
@@ -5,7 +5,6 @@ import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
@@ -18,11 +17,13 @@ import org.springframework.retry.annotation.EnableRetry
 @EnableRetry
 @EnableKafka
 class AAPSøknadApiApplication {
-    fun main(args: Array<String>) {
-        SpringApplicationBuilder(AAPSøknadApiApplication::class.java)
-            .profiles(*profiler())
-            .applicationStartup(BufferingApplicationStartup(4096))
-            .main(AAPSøknadApiApplication::class.java)
-            .run(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplicationBuilder(AAPSøknadApiApplication::class.java)
+                .profiles(*profiler())
+                .main(AAPSøknadApiApplication::class.java)
+                .run(*args)
+        }
     }
 }
