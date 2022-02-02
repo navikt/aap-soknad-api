d015864ce4135ae586efa2b927632c467502580c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-02-02 11:27:46 +0100

    narrow down

--- src/main/kotlin/no/nav/aap/api/oppslag/system/TestIt.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/system/TestIt.kt
@@ -1,8 +1,10 @@
 package no.nav.aap.api.oppslag.system
 
+import no.nav.aap.rest.AbstractRestConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.net.URI
 
 @ConfigurationProperties(prefix = "sts")
 @ConstructorBinding
-class TestIt (val username: String, val password: String)
+class TestIt (val username: String, val password: String) : AbstractRestConfig(URI("http://www.vg.no"), "ja", true)
