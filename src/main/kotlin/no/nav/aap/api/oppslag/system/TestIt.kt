87ee46123d0718c08ee94d5784e3a93bc64261eb
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-02-02 11:40:19 +0100

    narrow

--- src/main/kotlin/no/nav/aap/api/oppslag/system/TestIt.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/system/TestIt.kt
@@ -3,8 +3,10 @@ package no.nav.aap.api.oppslag.system
 import no.nav.aap.rest.AbstractRestConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.bind.DefaultValue
 import java.net.URI
+
 
 @ConfigurationProperties(prefix = "sts")
 @ConstructorBinding
-class TestIt (val username: String, val password: String) : AbstractRestConfig(URI("http://www.vg.no"), "ja", true)
+class TestIt (val username: String, val password: String, @DefaultValue("true") val enabled: Boolean) : AbstractRestConfig(URI("http://www.vg.no"), "ja", enabled)
