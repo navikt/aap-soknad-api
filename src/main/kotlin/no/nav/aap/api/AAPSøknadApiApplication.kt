c059707d7fc5f87dbb2c027a364fc00ffca8c565
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-02-03 13:58:23 +0100

    hent token

--- src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
+++ src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
@@ -1,14 +1,20 @@
 package no.nav.aap.api
 
 import no.nav.boot.conditionals.Cluster.profiler
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.apache.commons.logging.LogFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
+import org.springframework.stereotype.Component
 
 
 @SpringBootApplication
@@ -20,8 +26,20 @@ import org.springframework.retry.annotation.EnableRetry
 class AAPSøknadApiApplication
 
 fun main(args: Array<String>) {
+
     runApplication<AAPSøknadApiApplication>(*args) {
         setAdditionalProfiles(*profiler())
         applicationStartup = BufferingApplicationStartup(4096)
+    }
+
+    @Component
+    class ApplicationStartupRunner(private val configs: ClientConfigurationProperties, private val service: OAuth2AccessTokenService) : CommandLineRunner {
+         val logger = LogFactory.getLog(javaClass)
+        @Throws(Exception::class)
+        override fun run(vararg args: String) {
+            logger.info("XXX  henter token")
+            val token = service.getAccessToken(configs.registration["client-credentials-pdl"]).accessToken
+            logger.info("XXX  token er  $token")
+        }
     }
 }
