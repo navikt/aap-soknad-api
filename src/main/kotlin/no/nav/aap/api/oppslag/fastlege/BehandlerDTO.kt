33c31506ddc0c005a0eca0ee287a648eb21d91af
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-09 13:20:57 +0100

    bump

--- src/main/kotlin/no/nav/aap/api/oppslag/fastlege/BehandlerDTO.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/fastlege/BehandlerDTO.kt
@@ -1,22 +1,28 @@
 package no.nav.aap.api.oppslag.fastlege
 
+import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.Navn
+import no.nav.aap.api.felles.OrgNummer
 
 data class BehandlerDTO(
-        val type: String,
+        val type: BehandlerType,
         val behandlerRef: String,
-        val fnr: String?,
+        val fnr: Fødselsnummer?,
         val fornavn: String,
         val mellomnavn: String?,
         val etternavn: String,
-        val orgnummer: String?,
+        val orgnummer: OrgNummer?,
         val kontor: String?,
         val adresse: String?,
         val postnummer: String?,
         val poststed: String?,
         val telefon: String?)
 
-data class Fastlege(val navn: Navn?) // TODO mer
+enum class BehandlerType {
+    FASTLEGE
+}
+
+data class Fastlege(val navn: Navn) // TODO mer
 
 
 fun BehandlerDTO.tilFastlege() = Fastlege(Navn(fornavn,mellomnavn,etternavn)) // TODO
