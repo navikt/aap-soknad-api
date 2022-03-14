f818a32aab76d9ac48f84cece845f131a3247308
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-14 16:01:13 +0100

    bump, anonymize

--- src/test/kotlin/no/nav/aap/api/oppslag/fastlege/FastlegeTest.kt
+++ src/test/kotlin/no/nav/aap/api/oppslag/fastlege/FastlegeTest.kt
@@ -3,7 +3,9 @@ package no.nav.aap.api.oppslag.fastlege
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.OrgNummer
+import no.nav.aap.api.oppslag.fastlege.BehandlerType.FASTLEGE
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,13 +19,13 @@ class FastlegeTest {
             "   {\n" +
             "      \"type\":\"FASTLEGE\",\n" +
             "      \"behandlerRef\":\"d182f24b-ebca-4f44-bf86-65901ec6141b\",\n" +
-            "      \"fnr\":\"01117302624\",\n" +
-            "      \"fornavn\":\"Nina Unni\",\n" +
+            "      \"fnr\":\"01010111111\",\n" +
+            "      \"fornavn\":\"Unni\",\n" +
             "      \"mellomnavn\":\"\",\n" +
-            "      \"etternavn\":\"Borge\",\n" +
+            "      \"etternavn\":\"Larsen\",\n" +
             "      \"orgnummer\":\"976673867\",\n" +
-            "      \"kontor\":\"ASKØY KOMMUNE SAMFUNNSMEDISINSK AVD ALMENNLEGETJENESTEN\",\n" +
-            "      \"adresse\":\"Kleppeveien 17\",\n" +
+            "      \"kontor\":\"Legesenteret AS\",\n" +
+            "      \"adresse\":\"Legeveien 17\",\n" +
             "      \"postnummer\":\"5300\",\n" +
             "      \"poststed\":\"KLEPPESTØ\",\n" +
             "      \"telefon\":\"500000230\"\n" +
@@ -33,11 +35,14 @@ class FastlegeTest {
     fun serdeserTest() {
         val o = OrgNummer("976673867")
         serdeser(o)
+        val f = BehandlerDTO(FASTLEGE,"123", Fødselsnummer("11111111111"),"Unni", "Mellom","Larsen",
+                OrgNummer("976673867"),"Kontor","Adresse","5300","KLEPPESTØ","61253479")
+        serdeser(f,true)
     }
     private fun serdeser(a: Any, print: Boolean = false) {
         mapper.registerKotlinModule()
         mapper.registerModule(JavaTimeModule())
-        val ser = mapper.writeValueAsString(a)
+        val ser = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(a)
         if (print) println(ser)
         val deser = mapper.readValue(ser, a::class.java)
         if (print) println(deser)
