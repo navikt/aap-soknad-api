6251541d2018cf7508e0d105aff90670f8bbd1a7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-16 16:33:16 +0100

    log payload

--- src/main/kotlin/no/nav/aap/api/oppslag/organisasjon/OrganisasjonClientBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/organisasjon/OrganisasjonClientBeanConfig.kt
@@ -3,19 +3,24 @@ package no.nav.aap.api.oppslag.organisasjon
 import no.nav.aap.api.oppslag.organisasjon.OrganisasjonConfig.Companion.ORGANISASJON
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.rest.AbstractWebClientAdapter.Companion.correlatingFilterFunction
+import no.nav.boot.conditionals.EnvUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import reactor.netty.http.client.HttpClient
 
 @Configuration
 class OrganisasjonClientBeanConfig(@Value("\${spring.application.name}") val applicationName: String, val cfg: OrganisasjonConfig) {
 
     @Bean
     @Qualifier(ORGANISASJON)
-    fun organisasjonkWebClient(builder: WebClient.Builder) =
+    fun organisasjonkWebClient(builder: WebClient.Builder,env: Environment) =
         builder
+            .clientConnector(ReactorClientHttpConnector(HttpClient.create().wiretap(EnvUtil.isDevOrLocal(env))))
             .baseUrl(cfg.baseUri.toString())
             .filter(correlatingFilterFunction(applicationName))
             .build()
