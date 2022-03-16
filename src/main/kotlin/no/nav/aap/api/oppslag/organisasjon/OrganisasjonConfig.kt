962067890cfdb7acf7954dbeca81137819491f51
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-16 16:14:25 +0100

    ping

--- src/main/kotlin/no/nav/aap/api/oppslag/organisasjon/OrganisasjonConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/organisasjon/OrganisasjonConfig.kt
@@ -13,12 +13,13 @@ import org.springframework.web.util.UriComponentsBuilder.newInstance
 @ConstructorBinding
 class OrganisasjonConfig (baseUri: URI,
                           @DefaultValue(ORGPATH) private val organisasjonPath: String,
+                          @DefaultValue(PINGPATH)  val pp: String,
                           @DefaultValue("true") enabled: Boolean) :
-    AbstractRestConfig(baseUri, pingPath(organisasjonPath), enabled) {
+    AbstractRestConfig(baseUri, pp, enabled) {
     fun getOrganisasjonURI(b: UriBuilder, orgnr: String?): URI {
         return b.path(organisasjonPath)
             .queryParam("orgnummer", orgnr)
-            .build(orgnr)
+            .build()
     }
 
     override fun toString(): String {
@@ -28,13 +29,6 @@ class OrganisasjonConfig (baseUri: URI,
     companion object {
         const val ORGANISASJON = "Organisasjon"
         private const val ORGPATH = "organisasjon"
-        private const val NAV = "998004993"
-        private fun pingPath(organisasjonPath: String): String {
-            return newInstance()
-                .path(organisasjonPath)
-                .queryParam("orgnummer", NAV)
-                .build()
-                .toString()
-        }
+        private const val PINGPATH = ORGPATH + "/ping"
     }
 }
