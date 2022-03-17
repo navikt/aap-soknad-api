514259441d924964ac55832c3d98ddf5eadda25c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-17 11:08:11 +0100

    orgnavn

--- src/main/kotlin/no/nav/aap/api/oppslag/organisasjon/OrganisasjonClient.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.aap.api.oppslag.organisasjon
-
-import org.springframework.stereotype.Component
-
-@Component
-class OrganisasjonClient(private val adapter: OrganisasjonWebClientAdapter) {
-    fun orgNavn(orgnr: String?) = adapter.orgNavn(orgnr)
-}
