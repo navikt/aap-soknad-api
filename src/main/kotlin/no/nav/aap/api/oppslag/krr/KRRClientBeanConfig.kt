e5dedf549e41cd066528028f414df9f0e16f67d8
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-18 21:15:23 +0100

    felles

--- src/main/kotlin/no/nav/aap/api/oppslag/krr/KRRClientBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/krr/KRRClientBeanConfig.kt
@@ -2,7 +2,7 @@ package no.nav.aap.api.oppslag.krr
 
 import no.nav.aap.api.oppslag.krr.KRRConfig.Companion.KRR
 import no.nav.aap.health.AbstractPingableHealthIndicator
-import no.nav.aap.rest.AbstractWebClientAdapter
+import no.nav.aap.rest.AbstractWebClientAdapter as felles
 import no.nav.aap.rest.tokenx.TokenXFilterFunction
 import no.nav.aap.util.AuthContext
 import no.nav.aap.util.Constants
@@ -13,9 +13,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.web.reactive.function.client.ClientRequest
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
-import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient.Builder
 import reactor.netty.http.client.HttpClient
 
@@ -28,21 +25,12 @@ class KRRClientBeanConfig(@Value("\${spring.application.name}") val applicationN
         builder
             .clientConnector(ReactorClientHttpConnector(HttpClient.create().wiretap(isDevOrLocal(env))))
             .baseUrl(cfg.baseUri.toString())
-            .filter(AbstractWebClientAdapter.correlatingFilterFunction(applicationName))
-            .filter(navPersonIdentFilterFunction(Constants.NAV_PERSON_IDENT) { ctx.getSubject() ?: "unauthenticated" })
+            .filter(felles.correlatingFilterFunction(applicationName))
+            .filter(felles.generellFilterFunction(Constants.NAV_PERSON_IDENT) { ctx.getSubject() ?: "unauthenticated" })
             .filter(tokenXFilterFunction)
             .build()
 
     @Bean
     fun krrHealthIndicator(a: KRRWebClientAdapter) = object: AbstractPingableHealthIndicator(a){
     }
-
-    fun navPersonIdentFilterFunction(key: String, value: () -> String) =
-        ExchangeFilterFunction { req: ClientRequest, next: ExchangeFunction ->
-            next.exchange(
-                    ClientRequest.from(req)
-                        .header(key, value.invoke())
-                        .build())
-        }
-
 }
