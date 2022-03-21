e393dc94d291c802cd7586920fa4c2848b1a53b4
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-21 15:17:04 +0100

    målform

--- src/main/kotlin/no/nav/aap/api/oppslag/OppslagController.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/OppslagController.kt
@@ -16,17 +16,9 @@ class OppslagController(val pdl: PDLClient, val behandler: BehandlerClient, val 
 
     @GetMapping("/soeker")
     fun søker() : SøkerInfo {
-        log.info("Slår opp arbeidsforhold")
         val arbeidsforhold = arbeid.arbeidsforhold()
-        log.info("Arbeidsforhold $arbeidsforhold")
-
-        log.info("Slår opp behandlere")
         val behandlere = behandler.behandlere()
-        log.info("Slått opp behandlere $behandlere")
-
-        log.info("Slår opp målform")
-        val mf = krr.målform()
-        log.info("Målform $mf")
-        return SøkerInfo(pdl.søkerMedBarn(), behandlere, arbeidsforhold)
+        val målform = krr.målform()
+        return SøkerInfo(pdl.søkerMedBarn(), behandlere, arbeidsforhold, målform)
     }
 }
--- src/main/kotlin/no/nav/aap/api/oppslag/SøkerInfo.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/SøkerInfo.kt
@@ -2,6 +2,7 @@ package no.nav.aap.api.oppslag
 
 import no.nav.aap.api.oppslag.arbeidsforhold.ArbeidsforholdDTO.Arbeidsforhold
 import no.nav.aap.api.oppslag.behandler.Behandler
+import no.nav.aap.api.oppslag.krr.Målform
 import no.nav.aap.api.søknad.model.Søker
 
-data class SøkerInfo(val søker: Søker?, val behandlere: List<Behandler>,val arbeidsforhold: List<Arbeidsforhold>)
+data class SøkerInfo(val søker: Søker?, val behandlere: List<Behandler>,val arbeidsforhold: List<Arbeidsforhold>, val målform: Målform)
