6484d35811690500fbd505da94a1b56aa8397109
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-21 09:34:41 +0100

    ignore

--- src/main/kotlin/no/nav/aap/api/oppslag/krr/KontaktinformasjonDTO.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/krr/KontaktinformasjonDTO.kt
@@ -1,8 +1,11 @@
 package no.nav.aap.api.oppslag.krr
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Kontaktinformasjon(@JsonProperty("spraak") val  målform: Målform)
 
 enum class Målform {
