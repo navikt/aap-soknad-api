0c5779bb58fc5d04c0f18de9b85d78984f95e26a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-28 11:46:43 +0200

    default nb if null

--- src/main/kotlin/no/nav/aap/api/oppslag/krr/KontaktinformasjonDTO.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/krr/KontaktinformasjonDTO.kt
@@ -2,11 +2,12 @@ package no.nav.aap.api.oppslag.krr
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-
+import no.nav.aap.api.oppslag.krr.Målform.Companion
+import no.nav.aap.api.oppslag.krr.Målform.NB
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Kontaktinformasjon(@JsonProperty("spraak") val  målform: Målform)
+class Kontaktinformasjon(@JsonProperty("spraak") val  målform: Målform? = Målform.standard())
 
 enum class Målform {
     NB,NN,EN;
