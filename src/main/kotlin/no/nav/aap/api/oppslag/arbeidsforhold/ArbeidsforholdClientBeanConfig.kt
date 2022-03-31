267421abdc067c155f00ee14890c25c0f141e4ea
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-31 20:11:34 +0200

    use generellFilterFunction

--- src/main/kotlin/no/nav/aap/api/oppslag/arbeidsforhold/ArbeidsforholdClientBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/arbeidsforhold/ArbeidsforholdClientBeanConfig.kt
@@ -2,12 +2,13 @@ package no.nav.aap.api.oppslag.arbeidsforhold
 
 import no.nav.aap.api.oppslag.arbeidsforhold.ArbeidsforholdConfig.Companion.ARBEIDSFORHOLD
 import no.nav.aap.health.AbstractPingableHealthIndicator
+import no.nav.aap.rest.AbstractWebClientAdapter.Companion.generellFilterFunction
 import no.nav.aap.rest.AbstractWebClientAdapter.Companion.correlatingFilterFunction
-import no.nav.aap.rest.MDCPropagatingFilterFunction
 import no.nav.aap.rest.tokenx.TokenXFilterFunction
 import no.nav.aap.util.AuthContext
 import no.nav.aap.util.Constants
 import no.nav.boot.conditionals.EnvUtil.isDevOrLocal
+import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -15,8 +16,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.ClientRequest
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
-import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient.Builder
 import reactor.netty.http.client.HttpClient
 
@@ -36,12 +35,10 @@ class ArbeidsforholdClientBeanConfig(@Value("\${spring.application.name}") val a
             .build()
 
 
-    private fun navPersonIdentFunction(ctx: AuthContext) =
-         ExchangeFilterFunction { req: ClientRequest, next: ExchangeFunction ->
-            next.exchange(ClientRequest.from(req)
-                        .header(Constants.NAV_PERSON_IDENT, ctx.getSubject())
-                        .build())
+    private fun navPersonIdentFunction(ctx: AuthContext) = generellFilterFunction(Constants.NAV_PERSON_IDENT) {
+        ctx.getSubject() ?: throw JwtTokenMissingException()
     }
+
     @Bean
     fun arbeidsforholdHealthIndicator(a: ArbeidsforholdClientAdapter) = object: AbstractPingableHealthIndicator(a){
     }
