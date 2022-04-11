ece5f064b6438533c2a588d865729dc72875398f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-11 21:39:12 +0200

    lagre

--- src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
@@ -1,14 +1,11 @@
 package no.nav.aap.api.mellomlagring
 
-import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo.newBuilder
 import com.google.cloud.storage.Storage
 import no.nav.aap.api.felles.Fødselsnummer
-import no.nav.aap.api.søknad.SkjemaType
 import no.nav.aap.util.LoggerUtil
 import no.nav.boot.conditionals.ConditionalOnGCP
-import org.apache.tika.Tika
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
@@ -16,22 +13,20 @@ import java.util.Objects.hash
 
 
 @ConditionalOnGCP
-class GCPVedlegg(@Value("\${mellomlagring.bucket:aap-vedlegg}") val bøttenavn: String,
-                 val storage: Storage)  {
+class GCPVedlegg(@Value("\${mellomlagring.bucket:aap-vedlegg}") val bøttenavn: String, val storage: Storage)  {
 
     val log = LoggerUtil.getLogger(javaClass)
      fun lagre(fnr: Fødselsnummer, file: MultipartFile): UUID {
          log.info("Lagrer vedlegg fra ${file.originalFilename}")
          val uuid = UUID.randomUUID()
          storage.create(
-                 newBuilder(BlobId.of(bøttenavn, key(fnr, uuid)))
+                 newBuilder(BlobId.of(bøttenavn, "${hash(fnr, uuid)}"))
                     .setContentType(file.contentType)
                     .build(), file.bytes)
         return uuid
     }
 
-    fun les(fnr: Fødselsnummer, uuid: UUID) = storage.get(bøttenavn, key(fnr, uuid))
+    fun les(fnr: Fødselsnummer, uuid: UUID) = storage.get(bøttenavn, "${hash(fnr, uuid)}")
 
-    fun slett(fnr: Fødselsnummer,uuid: UUID) = storage.delete(BlobId.of(bøttenavn, key(fnr,uuid)))
-    private fun key(fnr: Fødselsnummer,  uuid: UUID) = hash(fnr, uuid).toString()
+    fun slett(fnr: Fødselsnummer,uuid: UUID) = storage.delete(BlobId.of(bøttenavn, "${hash(fnr, uuid)}"))
 }
