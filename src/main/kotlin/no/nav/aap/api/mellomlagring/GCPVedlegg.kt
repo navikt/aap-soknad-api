8cb4053f3efaa1d863de1d95051570f32caf8a55
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-11 21:08:41 +0200

    lagre

--- src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.api.mellomlagring
 
+import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo.newBuilder
 import com.google.cloud.storage.Storage
@@ -9,6 +10,7 @@ import no.nav.aap.util.LoggerUtil
 import no.nav.boot.conditionals.ConditionalOnGCP
 import org.apache.tika.Tika
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.multipart.MultipartFile
 import java.util.*
 import java.util.Objects.hash
 
@@ -18,20 +20,17 @@ class GCPVedlegg(@Value("\${mellomlagring.bucket:aap-vedlegg}") val bøttenavn: 
                  val storage: Storage)  {
 
     val log = LoggerUtil.getLogger(javaClass)
-     fun lagre(fnr: Fødselsnummer, contentType: String?,bytes: ByteArray): UUID {
+     fun lagre(fnr: Fødselsnummer, file: MultipartFile): UUID {
          log.info("Lagrer vedlegg")
          val uuid = UUID.randomUUID()
          storage.create(
                  newBuilder(BlobId.of(bøttenavn, key(fnr, uuid)))
-                    .setContentType(contentType)
-                    .build(), bytes)
+                    .setContentType(file.contentType)
+                    .build(), file.bytes)
         return uuid
     }
 
-    fun les(fnr: Fødselsnummer, uuid: UUID) : Pair<String,ByteArray>?{
-        val data = storage.get(bøttenavn, key(fnr, uuid))
-        return data?.let { Pair(it.contentType,it.getContent()) }
-    }
+    fun les(fnr: Fødselsnummer, uuid: UUID) = storage.get(bøttenavn, key(fnr, uuid))
 
     fun slett(fnr: Fødselsnummer,uuid: UUID) = storage.delete(BlobId.of(bøttenavn, key(fnr,uuid)))
     private fun key(fnr: Fødselsnummer,  uuid: UUID) = hash(fnr, uuid).toString()
--- src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
@@ -4,7 +4,10 @@ import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.mellomlagring.GCPVedlegg
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpHeaders.CACHE_CONTROL
 import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
+import org.springframework.http.HttpHeaders.EXPIRES
+import org.springframework.http.HttpHeaders.PRAGMA
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.NO_CONTENT
@@ -12,6 +15,7 @@ import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
 import org.springframework.http.MediaType.parseMediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.MimeTypeUtils.parseMimeType
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -30,24 +34,24 @@ class VedleggDevController(private val vedlegg: GCPVedlegg) {
 
     @PostMapping(value = ["lagre/{fnr}"], consumes = [MULTIPART_FORM_DATA_VALUE])
     fun lagreVedlegg(@PathVariable fnr: Fødselsnummer, @RequestPart("vedlegg") file: MultipartFile): ResponseEntity<UUID> {
-        val uuid  = vedlegg.lagre(fnr, file.contentType,file.bytes)
+        val uuid  = vedlegg.lagre(fnr, file)
         return ResponseEntity<UUID>(uuid, CREATED)
     }
     @GetMapping(path= ["les/{fnr}/{uuid}"])
     fun lesVedlegg(@PathVariable fnr: Fødselsnummer,@PathVariable uuid: UUID) : ResponseEntity<ByteArray>?{
         val data = vedlegg.les(fnr, uuid)
         return data?.let {  ResponseEntity<ByteArray>(
-                data.second,
+                data.getContent(),
                 HttpHeaders().apply {
-                    add("Expires", "0")
-                    add("Pragma", "no-cache")
-                    add("Cache-Control", "no-cache, no-store, must-revalidate")
-                    add(CONTENT_DISPOSITION,"attachment; filename=data.pdf")
-                    contentType = parseMediaType(data.first) },
+                    add(EXPIRES, "0")
+                    add(PRAGMA, "no-cache")
+                    add(CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+                    add(CONTENT_DISPOSITION,"attachment; filename=data.${extensionFra(data.contentType)}")
+                    contentType = parseMediaType(data.contentType) },
                 OK)} ?: ResponseEntity<ByteArray>(NOT_FOUND)
     }
 
-
+    private fun extensionFra(mimeType: String) = parseMimeType(mimeType)?.subtype.lowercase()
 
     @DeleteMapping("slett/{fnr}/{uuid}")
     fun slettVedlegg(@PathVariable fnr: Fødselsnummer,@PathVariable uuid: UUID): ResponseEntity<Void> {
