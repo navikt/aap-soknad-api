1d242b9e8d5f1e22456a1e227783a1f3f51f977c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-11 15:58:17 +0200

    unprotected

--- src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
@@ -1,8 +1,7 @@
 package no.nav.aap.api.mellomlagring.dev
 
+import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.mellomlagring.GCPVedlegg
-import no.nav.aap.api.søknad.AuthContextExtension.getFnr
-import no.nav.aap.util.AuthContext
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NO_CONTENT
@@ -22,22 +21,22 @@ import org.springframework.web.bind.annotation.RestController
 @Unprotected
 @RestController
 @RequestMapping(value= ["/dev/jalla/"])
-class VedleggDevController(private val vedlegg: GCPVedlegg, private val ctx: AuthContext) {
+class VedleggDevController(private val vedlegg: GCPVedlegg) {
 
     @Unprotected
-    @PostMapping(value = ["lagre"], consumes = [MULTIPART_FORM_DATA_VALUE])
-    fun lagreVedlegg(@RequestPart("vedlegg") file: MultipartFile): ResponseEntity<UUID> {
-        val uuid  = vedlegg.lagre(ctx.getFnr(), file.contentType,file.bytes)
+    @PostMapping(value = ["lagre/{fnr}"], consumes = [MULTIPART_FORM_DATA_VALUE])
+    fun lagreVedlegg(@PathVariable fnr: Fødselsnummer, @RequestPart("vedlegg") file: MultipartFile): ResponseEntity<UUID> {
+        val uuid  = vedlegg.lagre(fnr, file.contentType,file.bytes)
         return ResponseEntity<UUID>(uuid, CREATED)
     }
     @Unprotected
-    @GetMapping("les/{uuid}")
-    fun lesVedlegg(@PathVariable uuid: UUID) = vedlegg.les(ctx.getFnr(), uuid)
+    @GetMapping("les/{fnr}/{uuid}")
+    fun lesVedlegg(@PathVariable fnr: Fødselsnummer,,@PathVariable uuid: UUID) = vedlegg.les(fnr, uuid)
 
     @Unprotected
-    @DeleteMapping("slett/{uuid}")
-    fun slettVedlegg(@PathVariable uuid: UUID): ResponseEntity<Void> {
-        vedlegg.slett(ctx.getFnr(),uuid)
+    @DeleteMapping("slett/{fnr}/{uuid}")
+    fun slettVedlegg(@PathVariable fnr: Fødselsnummer,@PathVariable uuid: UUID): ResponseEntity<Void> {
+        vedlegg.slett(fnr,uuid)
         return ResponseEntity<Void>(NO_CONTENT)
     }
 }
