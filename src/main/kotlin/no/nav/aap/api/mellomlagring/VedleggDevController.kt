4fb0cf023e7c9481d6d7a718c281256e3c20b61b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-11 14:42:58 +0200

    unprotected

--- src/main/kotlin/no/nav/aap/api/mellomlagring/VedleggDevController.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/VedleggDevController.kt
@@ -1,11 +1,8 @@
 package no.nav.aap.api.mellomlagring
 
 import no.nav.aap.api.søknad.AuthContextExtension.getFnr
-import no.nav.aap.api.søknad.SkjemaType
 import no.nav.aap.rest.UnprotectedRestController
 import no.nav.aap.util.AuthContext
-import no.nav.aap.util.Constants.IDPORTEN
-import no.nav.security.token.support.spring.ProtectedRestController
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
@@ -14,13 +11,12 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
 
-@UnprotectedRestController(value = ["/dev/vedlegg"])
+@UnprotectedRestController("/dev/vedlegg")
 class VedleggDevController(private val vedlegg: GCPVedlegg, private val ctx: AuthContext) {
 
     @PostMapping(value = ["/lagre"], consumes = [MULTIPART_FORM_DATA_VALUE])
