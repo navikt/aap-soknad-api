e2ee34ff014f3ac43b1a8e8181aebfeb8f62a959
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-12 20:16:15 +0200

    joutnalfør vedlegg

--- src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/GCPVedlegg.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.api.mellomlagring
 
+import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo.newBuilder
 import com.google.cloud.storage.Storage
@@ -10,26 +11,27 @@ import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.boot.conditionals.ConditionalOnGCP
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.multipart.MultipartFile
-import java.util.UUID
+import java.util.*
 import java.util.Objects.hash
 
 
 @ConditionalOnGCP
 class GCPVedlegg(@Value("\${mellomlagring.bucket:aap-vedlegg}") private val bøttenavn: String, private val storage: Storage)  {
 
-     fun lagre(fnr: Fødselsnummer, file: MultipartFile): UUID {
+     fun lagre(fnr: Fødselsnummer, vedlegg: MultipartFile): UUID {
          val uuid = UUID.randomUUID()
          storage.create(
                  newBuilder(BlobId.of(bøttenavn, "${hash(fnr, uuid)}"))
-                     .setContentType(file.contentType)
-                     .setMetadata(mapOf(FILNAVN to file.originalFilename))
-                     .build(), file.bytes)
+                     .setContentType(vedlegg.contentType)
+                     .setMetadata(mapOf(FILNAVN to vedlegg.originalFilename))
+                     .build(), vedlegg.bytes)
         return uuid
     }
 
     fun les(fnr: Fødselsnummer, uuid: UUID) = storage.get(bøttenavn, "${hash(fnr, uuid)}", fields(METADATA, CONTENT_TYPE))
 
     fun slett(fnr: Fødselsnummer,uuid: UUID) = storage.delete(BlobId.of(bøttenavn, "${hash(fnr, uuid)}"))
+
 
     companion object {
          const val FILNAVN = "filnavn"
--- src/main/kotlin/no/nav/aap/api/søknad/StandardSøknadFormidler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/StandardSøknadFormidler.kt
@@ -1,6 +1,8 @@
 package no.nav.aap.api.søknad
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.cloud.storage.Blob
+import no.nav.aap.api.mellomlagring.GCPVedlegg
 import no.nav.aap.api.oppslag.pdl.PDLClient
 import no.nav.aap.api.søknad.SkjemaType.HOVED
 import no.nav.aap.api.søknad.joark.JoarkClient
@@ -11,6 +13,7 @@ import no.nav.aap.joark.AvsenderMottaker
 import no.nav.aap.joark.Bruker
 import no.nav.aap.joark.Dokument
 import no.nav.aap.joark.DokumentVariant
+import no.nav.aap.joark.Filtype.JPG
 import no.nav.aap.joark.Filtype.JSON
 import no.nav.aap.joark.Filtype.PDFA
 import no.nav.aap.joark.Journalpost
@@ -18,10 +21,17 @@ import no.nav.aap.joark.VariantFormat.ARKIV
 import no.nav.aap.joark.VariantFormat.ORIGINAL
 import no.nav.aap.util.LoggerUtil
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType.APPLICATION_PDF_VALUE
+import org.springframework.http.MediaType.IMAGE_JPEG_VALUE
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
-class StandardSøknadFormidler(private val joark: JoarkClient, private val pdf: PDFGenerator, private val pdl: PDLClient, private val kafka: StandardSøknadKafkaFormidler) {
+class StandardSøknadFormidler(private val joark: JoarkClient,
+                              private val pdf: PDFGenerator,
+                              private val pdl: PDLClient,
+                              private val vedlegg: GCPVedlegg,
+                              private val kafka: StandardSøknadKafkaFormidler) {
 
     @Autowired
     private lateinit var mapper: ObjectMapper
@@ -31,7 +41,7 @@ class StandardSøknadFormidler(private val joark: JoarkClient, private val pdf: 
         with(pdl.søkerUtenBarn()) {
             joark.opprettJournalpost(
                     Journalpost(
-                            dokumenter = docs(this, søknad),
+                            dokumenter = docs(this, søknad).also { log.info("Journalfører ${it.size} dokumenter") },
                             tittel = HOVED.tittel,
                             avsenderMottaker = AvsenderMottaker(fødselsnummer, navn = navn.navn),
                             bruker = Bruker(fødselsnummer)))
@@ -40,8 +50,36 @@ class StandardSøknadFormidler(private val joark: JoarkClient, private val pdf: 
                 .also { log.info("Formidlet søknad til Kakfa OK") }
         }
 
-    private fun docs(søker: Søker, søknad: StandardSøknad)  =
-        listOf(Dokument(HOVED.tittel, HOVED.kode, listOf(
-                DokumentVariant(JSON, søknad.toEncodedJson(mapper), ORIGINAL),
-                DokumentVariant(PDFA, pdf.generate(søker, søknad), ARKIV))))
+    private fun docs(søker: Søker, søknad: StandardSøknad): List<Dokument> {
+         val andreStønader = søknad.utbetalinger?.stønadstyper
+           ?.filterNot { it.vedlegg == null }
+            ?.map { with(vedlegg.les(søker.fødselsnummer,it.vedlegg!!)) { dokumentVariant() } }
+             ?.filterNotNull()
+             .orEmpty()
+
+          val andreUtbetalinger = søknad.utbetalinger?.andreUtbetalinger
+              ?.filterNot { it.vedlegg == null }
+              ?.map { with(vedlegg.les(søker.fødselsnummer,it.vedlegg!!)) { dokumentVariant() } }
+              ?.filterNotNull()
+              .orEmpty()
+
+        return listOf(Dokument(
+                HOVED.tittel, HOVED.kode, listOf(
+                        DokumentVariant(JSON, søknad.toEncodedJson(mapper), ORIGINAL),
+                        DokumentVariant(PDFA, pdf.generate(søker, søknad), ARKIV))
+                  + andreStønader + andreUtbetalinger))
+    }
+
+    private fun Blob.dokumentVariant() =
+        when (contentType) {
+            APPLICATION_PDF_VALUE -> DokumentVariant(PDFA, encodedContent(), ARKIV)
+            IMAGE_JPEG_VALUE -> DokumentVariant(JPG, encodedContent(), ARKIV)
+            else -> {
+                log.warn("Contemt type $contentType er ukjent")
+                null
+            }
+    }
+
+    fun Blob.encodedContent() = Base64.getEncoder().encodeToString(getContent())
+
 }
