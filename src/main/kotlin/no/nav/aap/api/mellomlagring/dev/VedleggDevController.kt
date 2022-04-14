6d94f2742481ef6fa90594b34ce7a21b55e6a659
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-14 15:52:52 +0200

    chore

--- src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/dev/VedleggDevController.kt
@@ -3,19 +3,18 @@ package no.nav.aap.api.mellomlagring.dev
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.mellomlagring.GCPVedlegg
 import no.nav.aap.api.mellomlagring.GCPVedlegg.Companion.FILNAVN
+import no.nav.aap.api.mellomlagring.GCPVedlegg.Companion.FNR
 import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
+import org.springframework.http.CacheControl.noCache
+import org.springframework.http.ContentDisposition.attachment
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpHeaders.CACHE_CONTROL
-import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
-import org.springframework.http.HttpHeaders.EXPIRES
-import org.springframework.http.HttpHeaders.PRAGMA
-import org.springframework.http.HttpStatus.CREATED
-import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.NO_CONTENT
-import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
 import org.springframework.http.MediaType.parseMediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.notFound
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -31,33 +30,35 @@ import java.util.UUID
 @Unprotected
 @RestController
 @RequestMapping(value= ["/dev/vedlegg/"])
-class VedleggDevController(private val vedlegg: GCPVedlegg) {
+class VedleggDevController(private val bucket: GCPVedlegg) {
 
 
     @PostMapping(value = ["lagre/{fnr}"], consumes = [MULTIPART_FORM_DATA_VALUE])
     fun lagreVedlegg(@PathVariable fnr: Fødselsnummer, @RequestPart("vedlegg") file: MultipartFile): ResponseEntity<UUID> {
         val uuid = UUID.randomUUID()
-        return ResponseEntity.created(vedlegg.lagreVedlegg(fnr, uuid, file)).body(uuid)
+        return ResponseEntity.created(bucket.lagreVedlegg(fnr, uuid, file)).body(uuid)
     }
 
     @GetMapping(path= ["les/{fnr}/{uuid}"])
     fun lesVedlegg(@PathVariable fnr: Fødselsnummer,@PathVariable uuid: UUID) =
-         vedlegg.lesVedlegg(fnr, uuid)?.let {
-            ResponseEntity<ByteArray>(
-                    it.getContent(),
-                    HttpHeaders().apply {
-                        add(EXPIRES, "0")
-                        add(PRAGMA, "no-cache")
-                        add(CACHE_CONTROL, "no-cache, no-store, must-revalidate")
-                        add(CONTENT_DISPOSITION, "attachment; filename=${it.metadata[FILNAVN]}")
-                        contentType = parseMediaType(it.contentType)
-                    },
-                    OK)
-        } ?: ResponseEntity<ByteArray>(NOT_FOUND)
+        bucket.lesVedlegg(fnr, uuid)
+            ?.let { vedlegg ->
+                with(vedlegg) {
+                    if (fnr.fnr != metadata[FNR]) {
+                        throw JwtTokenUnauthorizedException("Dokumentet med id $uuid er ikke eid av $fnr.fnr")
+                    }
+                    ok()
+                        .contentType(parseMediaType(contentType))
+                        .cacheControl(noCache().mustRevalidate())
+                        .headers(HttpHeaders().apply {
+                            contentDisposition = attachment().filename(metadata[FILNAVN]!!).build() })
+                        .body(getContent())
+                }
+            } ?: notFound()
 
     @DeleteMapping("slett/{fnr}/{uuid}")
     fun slettVedlegg(@PathVariable fnr: Fødselsnummer,@PathVariable uuid: UUID): ResponseEntity<Void> {
-        vedlegg.slettVedlegg(fnr,uuid)
+        bucket.slettVedlegg(fnr,uuid)
         return ResponseEntity<Void>(NO_CONTENT)
     }
 }
