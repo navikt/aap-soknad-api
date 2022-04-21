50dd5db72c22ac128c678cf5b3e5a4cb3e63bb1d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-21 10:42:26 +0200

    ditt nav test

--- src/main/kotlin/no/nav/aap/api/søknad/dittnav/DittNavBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/dittnav/DittNavBeanConfig.kt
@@ -1,4 +1,7 @@
 package no.nav.aap.api.søknad.dittnav
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
@@ -20,5 +23,6 @@ class DittNavBeanConfig {
             .apply {
                 put(KEY_SERIALIZER_CLASS_CONFIG,KafkaAvroSerializer::class.java)
                 put(VALUE_SERIALIZER_CLASS_CONFIG,KafkaAvroSerializer::class.java)
+                put(SPECIFIC_AVRO_READER_CONFIG,true)
             }))
 }
