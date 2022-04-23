4c4007faa2c9ca87dfe93b1ed93a608e3c524b94
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-23 22:36:34 +0200

    chore

--- src/main/kotlin/no/nav/aap/api/søknad/dittnav/DittNavConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/dittnav/DittNavConfig.kt
@@ -2,13 +2,11 @@ package no.nav.aap.api.søknad.dittnav
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import java.net.URL
 import java.time.Duration
 @ConfigurationProperties(prefix = "dittnav")
 @ConstructorBinding
 data class DittNavConfig(val beskjed: TopicConfig) {
     data class TopicConfig(val topic: String,
-                           var landingsside: URL,
                            val varighet: Duration,
                            val enabled: Boolean = true)
 }
