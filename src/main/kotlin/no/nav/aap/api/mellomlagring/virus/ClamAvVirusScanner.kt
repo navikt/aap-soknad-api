4091efcb21dad7af52b5eb86583f7b81bddd37b3
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-24 22:35:29 +0200

    do not leak

--- src/main/kotlin/no/nav/aap/api/mellomlagring/virus/ClamAvVirusScanner.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/virus/ClamAvVirusScanner.kt
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Component
 
 @Component
 class ClamAvVirusScanner(private val a: VirusScanWebClientAdapter) : VirusScanner {
-    override fun harVirus(bytes: ByteArray, name: String?) =
+    override fun scan(bytes: ByteArray, name: String?) =
         if (a.harVirus(bytes,name)) {
             throw AttachmentVirusException(name)
             } else Unit
 }
 
 interface VirusScanner {
-     fun harVirus(bytes: ByteArray, name: String?)
+     fun scan(bytes: ByteArray, name: String?)
 }
