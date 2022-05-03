bc2e6b80c8fc55d9171b9d4a336dcaec6e4d262f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-03 11:52:07 +0200

    joark

--- src/main/kotlin/no/nav/aap/api/søknad/joark/JoarkConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/joark/JoarkConfig.kt
@@ -1,12 +1,13 @@
 package no.nav.aap.api.søknad.joark
 
 import no.nav.aap.rest.AbstractRestConfig
+import no.nav.aap.util.Constants.JOARK
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.bind.DefaultValue
 import java.net.URI
 
-@ConfigurationProperties("joark")
+@ConfigurationProperties(JOARK)
 @ConstructorBinding
 class JoarkConfig(
         @DefaultValue("/joark/aad") val joarkPath: String,
