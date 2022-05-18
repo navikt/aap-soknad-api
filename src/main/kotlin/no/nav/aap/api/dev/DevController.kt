46799f518effc6e521ded9d6f4ff469fc5c20255
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-18 23:28:41 +0200

    dev controller

--- src/main/kotlin/no/nav/aap/api/dev/DevController.kt
+++ src/main/kotlin/no/nav/aap/api/dev/DevController.kt
@@ -14,8 +14,8 @@ import no.nav.aap.api.søknad.model.StandardSøknad
 import no.nav.aap.api.søknad.model.Søker
 import no.nav.aap.api.søknad.routing.standard.StandardSøknadVLRouter
 import no.nav.aap.joark.JoarkResponse
-import no.nav.boot.conditionals.ConditionalOnDevOrLocal
-import no.nav.security.token.support.core.api.Unprotected
+import no.nav.boot.conditionals.ConditionalOnNotProd
+import no.nav.security.token.support.spring.UnprotectedRestController
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.springframework.http.CacheControl.noCache
 import org.springframework.http.ContentDisposition.attachment
@@ -32,56 +32,51 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import java.time.LocalDate.now
 import java.util.*
 
-@Unprotected
-@RestController
-@RequestMapping(value = ["/dev/"])
-@ConditionalOnDevOrLocal
+@UnprotectedRestController(["/dev/"])
+@ConditionalOnNotProd
 internal class DevController(private val dokumentLager: Dokumentlager,
                              private val mellomlager: Mellomlager,
                              private val dittnav: DittNavRouter,
                              private val vl: StandardSøknadVLRouter) {
 
-    @PostMapping(value = ["vl/{fnr}"])
+    @PostMapping("vl/{fnr}")
     @ResponseStatus(CREATED)
-    fun vl(@PathVariable fnr: Fødselsnummer,@RequestBody søknad: StandardSøknad) =
-        vl.route(søknad, Søker(Navn("Ole","B","Olsen"),fnr,
-                Adresse("Gata","A","14", PostNummer("2600","Lillehammer")),
+    fun vl(@PathVariable fnr: Fødselsnummer, @RequestBody søknad: StandardSøknad) =
+        vl.route(søknad, Søker(Navn("Ole", "B", "Olsen"), fnr,
+                Adresse("Gata", "A", "14", PostNummer("2600", "Lillehammer")),
                 now(), listOf()),
-                JoarkResponse("42",true, listOf()))
+                JoarkResponse("42", true, listOf()))
 
     @DeleteMapping("mellomlager/{type}/{fnr}")
     fun slettMellomlagret(@PathVariable type: SkjemaType, @PathVariable fnr: Fødselsnummer): ResponseEntity<Void> =
-        if (mellomlager.slett(fnr,type)) noContent().build() else notFound().build()
+        if (mellomlager.slett(fnr, type)) noContent().build() else notFound().build()
 
     @GetMapping("mellomlager/{type}/{fnr}")
     fun lesmMellomlagret(@PathVariable type: SkjemaType, @PathVariable fnr: Fødselsnummer) =
-        mellomlager.les(fnr, type) ?.let {ok(it)} ?: notFound().build()
+        mellomlager.les(fnr, type)?.let { ok(it) } ?: notFound().build()
 
     @PostMapping("mellomlager/{type}/{fnr}")
     @ResponseStatus(CREATED)
     fun mellomlagre(@PathVariable type: SkjemaType, @PathVariable fnr: Fødselsnummer, @RequestBody data: String) =
         mellomlager.lagre(fnr, type, data)
 
-    @PostMapping(value = ["dittnav/beskjed/{fnr}/{type}"])
+    @PostMapping("dittnav/beskjed/{fnr}/{type}")
     @ResponseStatus(CREATED)
-    fun opprettBeskjed(@PathVariable fnr: Fødselsnummer,@PathVariable type: SkjemaType) =
+    fun opprettBeskjed(@PathVariable fnr: Fødselsnummer, @PathVariable type: SkjemaType) =
         dittnav.opprettBeskjed(fnr, type)
 
     @PostMapping(value = ["vedlegg/lagre/{fnr}"], consumes = [MULTIPART_FORM_DATA_VALUE])
     @ResponseStatus(CREATED)
-    fun lagreDokument(@PathVariable fnr: Fødselsnummer, @RequestPart("vedlegg") vedlegg: MultipartFile) =
-        dokumentLager.lagreDokument(fnr, vedlegg.bytes, vedlegg.contentType, vedlegg.originalFilename)
+    fun lagreDokument(@PathVariable fnr: Fødselsnummer, @RequestPart("vedlegg") v: MultipartFile) =
+        dokumentLager.lagreDokument(fnr, v.bytes, v.contentType, v.originalFilename)
 
-
-    @GetMapping(path = ["vedlegg/les/{fnr}/{uuid}"])
+    @GetMapping("vedlegg/les/{fnr}/{uuid}")
     fun lesDokument(@PathVariable fnr: Fødselsnummer, @PathVariable uuid: UUID) =
         dokumentLager.lesDokument(fnr, uuid)
             ?.let {
