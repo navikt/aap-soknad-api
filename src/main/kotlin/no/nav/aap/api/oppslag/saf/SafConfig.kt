f91414d269392a2a30e8a17304ae446871cd944f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-23 13:25:45 +0200

    saf

--- src/main/kotlin/no/nav/aap/api/oppslag/saf/SafConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/saf/SafConfig.kt
@@ -24,7 +24,7 @@ class SafConfig(baseUri: URI,
 
     companion object {
         const val SAF = "saf"
-        private const val PINGPATH = "/isAlive"
-        private const val DOKPATH = "/rest/hentdokument/\${journalpostId}/\${dokumentInfoId}/\${variantFormat}"
+        private const val PINGPATH = "isAlive"
+        private const val DOKPATH = "rest/hentdokument/\${journalpostId}/\${dokumentInfoId}/\${variantFormat}"
     }
 }
