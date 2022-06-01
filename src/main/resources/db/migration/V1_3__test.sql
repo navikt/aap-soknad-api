0ef3af4a6c96431d068456deb81c01ec3b3f9027
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-01 09:52:38 +0200

    create table

---
+++ src/main/resources/db/migration/V1_3__test.sql
@@ -0,0 +1,1 @@
+GRANT USAGE ON SCHEMA PUBLIC TO "jan-olav.eide@nav.no";
--- src/main/resources/db/migration/V1_4__beskjed.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dittnavbeskjeder
-(
-    id      SERIAL PRIMARY KEY,
-    fnr     VARCHAR(50),
-    ref     VARCHAR(50),
-    created TIMESTAMP NOT NULL
-);
-DROP TABLE TEST;
-DROP TABLE TEST1;
