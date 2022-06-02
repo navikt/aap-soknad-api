45626b1346f8285556e85f3643301932f60416aa
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-02 11:07:22 +0200

    ereg direkte

--- src/main/kotlin/no/nav/aap/api/oppslag/arbeid/OrganisasjonWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/arbeid/OrganisasjonWebClientAdapter.kt
@@ -27,15 +27,21 @@ class OrganisasjonWebClientAdapter(@Qualifier(Constants.ORGANISASJON) val client
             .doOnSuccess { log.trace("Organisasjon oppslag OK") }
             .mapNotNull(OrganisasjonDTO::fulltNavn)
             .defaultIfEmpty(orgnr.orgnr)
-            .block()
+            .block() ?: orgnr.orgnr
             .also { log.trace("Organisasjon oppslag response $it") }
 
 }
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class OrganisasjonDTO(val navn : OrganisasjonNavnDTO) {
-    val fulltNavn =  with(navn) {
-        listOfNotNull(navnelinje1,navnelinje2,navnelinje3,navnelinje4,navnelinje5).joinToString(" ")
+data class OrganisasjonDTO(val navn: OrganisasjonNavnDTO) {
+    val fulltNavn = with(navn) {
+        listOfNotNull(navnelinje1, navnelinje2, navnelinje3, navnelinje4, navnelinje5).joinToString(" ")
     }
+
     @JsonIgnoreProperties(ignoreUnknown = true)
-    data class OrganisasjonNavnDTO(val navnelinje1: String?,val navnelinje2: String?,val navnelinje3: String?,val navnelinje4: String?,val navnelinje5: String?)
+    data class OrganisasjonNavnDTO(val navnelinje1: String?,
+                                   val navnelinje2: String?,
+                                   val navnelinje3: String?,
+                                   val navnelinje4: String?,
+                                   val navnelinje5: String?)
 }
