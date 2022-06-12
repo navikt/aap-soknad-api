3ceb978807125f8b587f43ad6a1a373b59d8673e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-12 22:14:28 +0200

    add nais config

--- src/main/kotlin/no/nav/aap/api/config/NAISConfig.kt
+++ src/main/kotlin/no/nav/aap/api/config/NAISConfig.kt
@@ -1,13 +1,12 @@
 package no.nav.aap.api.config
 
-import no.nav.boot.conditionals.ConditionalOnGCP
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.bind.DefaultValue
 
 @ConfigurationProperties("nais")
 @ConstructorBinding
-@ConditionalOnGCP
-class NAISConfig(val namespace: String/*, val app: NAISApp, val cluster: NAISCluster*/) /*{
+class NAISConfig(@DefaultValue("local") val namespace: String/*, val app: NAISApp, val cluster: NAISCluster*/) /*{
     data class NAISApp(val name: String, val image: String)
     data class NAISCluster(val name: String)
 }*/
