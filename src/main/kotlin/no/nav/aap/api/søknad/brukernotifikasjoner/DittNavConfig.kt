8188a22470d37390dc3f14d0bfa7ff1eeca9c1a1
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-12 22:43:44 +0200

    nais cfg

--- src/main/kotlin/no/nav/aap/api/søknad/brukernotifikasjoner/DittNavConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/brukernotifikasjoner/DittNavConfig.kt
@@ -3,16 +3,22 @@ package no.nav.aap.api.søknad.brukernotifikasjoner
 import no.nav.aap.api.søknad.brukernotifikasjoner.DittNavConfig.Companion.DITTNAV
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import java.time.Duration
 
 @ConfigurationProperties(DITTNAV)
 @ConstructorBinding
-data class DittNavConfig(val beskjed: TopicConfig, val oppgave: TopicConfig, val done: TopicConfig) {
+data class DittNavConfig(@NestedConfigurationProperty val config: NAISConfig,
+                         @NestedConfigurationProperty val beskjed: TopicConfig,
+                         @NestedConfigurationProperty val oppgave: TopicConfig,
+                         @NestedConfigurationProperty val done: TopicConfig) {
     data class TopicConfig(val topic: String,
                            val varighet: Duration = DEFAULT_DURATION,
                            val enabled: Boolean = true,
                            val sikkerhetsnivaa: Int = DEFAULT_LEVEL,
                            val eksternVarsling: Boolean = false)
+
+    data class NAISConfig(val namespace: String, val app: String)
 
     companion object {
         const val DITTNAV = "dittnav"
