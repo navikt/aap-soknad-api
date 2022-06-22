42b8cd212fe45e77addaa58b8fad8bf51dcfdd21
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-22 15:28:52 +0200

    krypter dokumentlager også

--- src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/api/error/AAPApiExceptionHandler.kt
@@ -3,6 +3,7 @@ package no.nav.aap.api.error
 import com.google.cloud.storage.StorageException
 import no.nav.aap.api.felles.error.IntegrationException
 import no.nav.aap.api.mellomlagring.virus.AttachmentException
+import no.nav.aap.util.LoggerUtil
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException
 import org.springframework.http.ResponseEntity
@@ -17,27 +18,27 @@ import org.zalando.problem.Status.UNAUTHORIZED
 import org.zalando.problem.Status.UNPROCESSABLE_ENTITY
 import org.zalando.problem.spring.web.advice.ProblemHandling
 
-
 @ControllerAdvice
 class AAPApiExceptionHandler : ProblemHandling {
+    private val log = LoggerUtil.getLogger(javaClass)
 
     @ExceptionHandler(JwtTokenUnauthorizedException::class, JwtTokenMissingException::class)
     fun handleMissingOrExpiredToken(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(UNAUTHORIZED, e, req)
+        create(UNAUTHORIZED, e, req).also { log.trace(UNAUTHORIZED.name, e) }
 
     @ExceptionHandler(IntegrationException::class)
     fun handleIntegrationException(e: IntegrationException, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(UNPROCESSABLE_ENTITY, e, req)
+        create(UNPROCESSABLE_ENTITY, e, req).also { log.trace(UNPROCESSABLE_ENTITY.name, e) }
 
     @ExceptionHandler(AttachmentException::class)
     fun handleVirus(e: AttachmentException, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(UNPROCESSABLE_ENTITY, e, req)
+        create(UNPROCESSABLE_ENTITY, e, req).also { log.trace(UNPROCESSABLE_ENTITY.name, e) }
 
     @ExceptionHandler(StorageException::class)
     fun handleStorageException(e: StorageException, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(BAD_REQUEST, e, req)
+        create(BAD_REQUEST, e, req).also { log.trace(BAD_REQUEST.name, e) }
 
     @ExceptionHandler(HttpClientErrorException.NotFound::class)
     fun handleNotFound(e: HttpClientErrorException, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(NOT_FOUND, e, req)
+        create(NOT_FOUND, e, req).also { log.trace(NOT_FOUND.name, e) }
 }
