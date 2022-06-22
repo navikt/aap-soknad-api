5f8ff7988c24c6cee1303447e265d2d38e159e4b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-22 10:20:19 +0200

    fjernet ubrukt

--- src/main/kotlin/no/nav/aap/api/mellomlagring/GCPKryptertMellomlager.kt
+++ src/main/kotlin/no/nav/aap/api/mellomlagring/GCPKryptertMellomlager.kt
@@ -4,25 +4,20 @@ import com.google.cloud.storage.BlobId.of
 import com.google.cloud.storage.BlobInfo.newBuilder
 import com.google.cloud.storage.Storage
 import com.google.crypto.tink.Aead
-import com.google.crypto.tink.KeyTemplates.get
-import com.google.crypto.tink.KeysetHandle.generateNew
-import com.google.crypto.tink.aead.KmsEnvelopeAeadKeyManager.createKeyTemplate
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.SkjemaType
 import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.boot.conditionals.ConditionalOnGCP
 import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
-import org.springframework.context.annotation.Primary
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import java.nio.charset.StandardCharsets.UTF_8
 
 @ConditionalOnGCP
-@Primary
 internal class GCPKryptertMellomlager(private val config: GCPBucketConfig,
                                       private val lager: Storage,
                                       private val aead: Aead) : Mellomlager {
     val log = getLogger(javaClass)
-    
+
     override fun lagre(fnr: Fødselsnummer, type: SkjemaType, value: String) =
         lager.create(newBuilder(of(config.bucket, key(fnr, type)))
             .setContentType(APPLICATION_JSON_VALUE).build(),
