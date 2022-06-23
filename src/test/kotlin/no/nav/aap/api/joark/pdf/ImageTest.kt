3c6ffa35f94fa57929a0ad80903e6f0dd9f04268
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-23 13:07:53 +0200

    flyttet og renamet Excptiopn

--- src/test/kotlin/no/nav/aap/api/joark/pdf/ImageTest.kt
+++ src/test/kotlin/no/nav/aap/api/joark/pdf/ImageTest.kt
@@ -2,7 +2,7 @@ package no.nav.aap.api.joark.pdf
 
 import junit.framework.TestCase.assertEquals
 import no.nav.aap.api.søknad.joark.pdf.Image2PDFConverter
-import no.nav.aap.api.søknad.virus.AttachmentException
+import no.nav.aap.api.søknad.mellomlagring.GCPBucketConfig.DokumentException
 import org.apache.tika.Tika
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -22,13 +22,13 @@ class ImageByteArray2PDFConverterTest {
     fun png2Pdf() = assertTrue(isPdf(converter.convert("pdf/nav-logo.png")))
 
     @Test
-    fun gifFeil() = assertThrows<AttachmentException> { converter.convert("pdf/loading.gif") }
+    fun gifFeil() = assertThrows<DokumentException> { converter.convert("pdf/loading.gif") }
 
     @Test
     fun pdf2Pdf() = assertEquals(APPLICATION_PDF_VALUE, TIKA.detect(converter.convert("pdf/test123.pdf")))
 
     @Test
-    fun junkFeil() = assertThrows<AttachmentException> { converter.convert(byteArrayOf(1, 2, 3, 4)) }
+    fun junkFeil() = assertThrows<DokumentException> { converter.convert(byteArrayOf(1, 2, 3, 4)) }
 
     companion object {
         private val TIKA = Tika()
