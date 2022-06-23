481a3a3301cff6e2d398da33640d99fba9d9b16b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-23 08:57:22 +0200

    ikke lekk blob ut

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/dokument/Dokumentlager.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/dokument/Dokumentlager.kt
@@ -17,6 +17,6 @@ interface Dokumentlager {
 }
 
 data class DokumentInfo(val bytes: ByteArray, val contentType: String?, val filnavn: String?) {
-    override fun toString() = "${javaClass.simpleName} [filtype=$filnavn,contentType=$contentType,bytes=${bytes.size} bytes]" }
-
+    override fun toString() =
+        "${javaClass.simpleName} [filtype=$filnavn,contentType=$contentType,bytes=${bytes.size} bytes]"
 }
