a894e9b0f5133f91a2ff6830b0b080bf281ed1d2
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-27 17:39:33 +0200

    log test

--- src/main/kotlin/no/nav/aap/api/dev/DevBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/dev/DevBeanConfig.kt
@@ -1,19 +1,27 @@
 package no.nav.aap.api.dev
 
 import no.nav.aap.rest.ActuatorIgnoringTraceRequestFilter
+import no.nav.aap.util.LoggerUtil
 import no.nav.boot.conditionals.ConditionalOnDevOrLocal
 import org.springframework.boot.actuate.trace.http.HttpExchangeTracer
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
 @Configuration
 @ConditionalOnDevOrLocal
 class DevBeanConfig
 
+val log = LoggerUtil.getLogger(DevBeanConfig::class.java)
+
 @Bean
-fun httpTraceRepository() = InMemoryHttpTraceRepository()
+fun httpTraceRepository(env: Environment): HttpTraceRepository {
+    log.info("DB ${env.getProperty("db.username")}")
+    log.info("DB ${env.getProperty("db.passwors")}")
+    return InMemoryHttpTraceRepository()
+}
 
 @Bean
 fun actuatorIgnoringTraceRequestFilter(repo: HttpTraceRepository, tracer: HttpExchangeTracer) =
