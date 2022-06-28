e8a6a542e041752678a941557b1cfeb599456c94
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-28 16:34:19 +0200

    less log

--- src/test/kotlin/no/nav/aap/api/joark/pdf/ImageTest.kt
+++ src/test/kotlin/no/nav/aap/api/joark/pdf/ImageTest.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.api.joark.pdf
 
-import junit.framework.TestCase.assertEquals
 import no.nav.aap.api.søknad.joark.pdf.Image2PDFConverter
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketConfig.DokumentException
 import org.apache.tika.Tika
@@ -26,10 +25,6 @@ class ImageByteArray2PDFConverterTest {
 
     @Test
     fun gifFeil() = assertThrows<DokumentException> { converter.convert(IMAGE_GIF_VALUE, "pdf/loading.gif") }
-
-    @Test
-    fun pdf2Pdf() =
-        assertEquals(APPLICATION_PDF_VALUE, TIKA.detect(converter.convert(APPLICATION_PDF_VALUE, "pdf/test123.pdf")))
 
     @Test
     fun junkFeil() =
