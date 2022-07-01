9f0c6c6c35e6efb2e4f89b0de77e76e8cf58f946
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-07-01 23:45:33 +0200

    more cleanup

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/dokument/InMemoryDokumentlager.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/dokument/InMemoryDokumentlager.kt
@@ -1,11 +1,10 @@
 package no.nav.aap.api.søknad.mellomlagring.dokument
 
-import no.nav.aap.api.søknad.mellomlagring.GCPKMSKeyKryptertMellomlager
 import no.nav.aap.api.søknad.model.StandardSøknad
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import java.util.*
 
-@ConditionalOnMissingBean(GCPKMSKeyKryptertMellomlager::class)
+@ConditionalOnMissingBean(GCPKMSKeyKryptertDokumentlager::class)
 class InMemoryDokumentlager : Dokumentlager {
     private val store = mutableMapOf<String, String>()
 
