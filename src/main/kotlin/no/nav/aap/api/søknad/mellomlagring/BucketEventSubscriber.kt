4a8f4f1b973e2caeb6a5330ed0c088c3c5b8bc16
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-07-04 20:25:04 +0200

    add uuid to metadata

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketEventSubscriber.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketEventSubscriber.kt
@@ -5,25 +5,24 @@ import com.google.cloud.pubsub.v1.MessageReceiver
 import com.google.cloud.pubsub.v1.Subscriber
 import com.google.pubsub.v1.ProjectSubscriptionName
 import com.google.pubsub.v1.PubsubMessage
+import no.nav.aap.util.LoggerUtil
+import org.springframework.stereotype.Component
 import java.util.concurrent.TimeoutException
 
-object BucketEventSubscriber {
-    @Throws(Exception::class)
-    @JvmStatic
-    fun main(args: Array<String>) {
+@Component
+class BucketEventSubscriber {
+    private val log = LoggerUtil.getLogger(javaClass)
+
+    fun subscribeAsyncExample() {
         val projectId = "aap-dev-e48b" // TODO fix
         val subscriptionId = "testsub"
-        subscribeAsyncExample(projectId, subscriptionId)
-    }
-
-    fun subscribeAsyncExample(projectId: String?, subscriptionId: String?) {
         val subscriptionName = ProjectSubscriptionName.of(projectId, subscriptionId)
 
         // Instantiate an asynchronous message receiver.
         val receiver = MessageReceiver { message: PubsubMessage, consumer: AckReplyConsumer ->
             // Handle incoming message, then ack the received message.
-            println("Id: " + message.messageId)
-            println("Data: " + message.attributesMap)
+            log.info("Id: ${message.messageId}")
+            log.info("Data: ${message.attributesMap}")
             consumer.ack()
         }
         var subscriber: Subscriber? = null
@@ -31,7 +30,7 @@ object BucketEventSubscriber {
             subscriber = Subscriber.newBuilder(subscriptionName, receiver).build()
             // Start the subscriber.
             subscriber.startAsync().awaitRunning()
-            System.out.printf("Listening for messages on %s:\n", subscriptionName.toString())
+            log.info("Listening for messages on %s:\n", subscriptionName.toString())
             // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
             subscriber.awaitRunning()
             while (true) {
