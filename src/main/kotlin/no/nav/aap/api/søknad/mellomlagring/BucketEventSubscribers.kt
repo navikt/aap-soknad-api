6c8941599cf4088d16489027c114af36bba8983b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-07-06 21:51:29 +0200

    log

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketEventSubscribers.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketEventSubscribers.kt
@@ -6,13 +6,15 @@ import com.google.cloud.storage.Storage
 import no.nav.boot.conditionals.ConditionalOnGCP
 
 @ConditionalOnGCP
-class VedleggEventSubscriber(private val storage: Storage, private val cfgs: BucketsConfig) :
-    AbstractEventSubscriber(storage, cfgs.vedlegg, cfgs.id) {
+class VedleggEventSubscriber(mapper: ObjectMapper, private val storage: Storage, private val cfgs: BucketsConfig) :
+    AbstractEventSubscriber(mapper, storage, cfgs.vedlegg, cfgs.id) {
 
     override fun receiver() =
         MessageReceiver { message, consumer ->
             log.info("Id: ${message.messageId}")
             log.info("Data: ${message.attributesMap}")
+            val resource = mapper.readValue(message.data.toStringUtf8(), Map::class.java)
+            log.info("Resource representation: $resource")
             consumer.ack()
         }
 }
