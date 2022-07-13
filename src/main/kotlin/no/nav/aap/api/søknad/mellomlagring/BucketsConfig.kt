6d4a8b436c3436dc5d0a4cfe9f6ff53d1dcf2c1b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-07-13 19:21:58 +0200

    reorg

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketsConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketsConfig.kt
@@ -3,6 +3,7 @@ package no.nav.aap.api.søknad.mellomlagring
 import com.google.cloud.kms.v1.CryptoKeyName
 import com.google.cloud.kms.v1.LocationName
 import no.nav.aap.api.søknad.mellomlagring.BucketsConfig.Companion.BUCKETS
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
@@ -14,14 +15,16 @@ import java.time.Duration
 data class BucketsConfig(@NestedConfigurationProperty val mellom: MellomlagringBucketConfig,
                          @NestedConfigurationProperty val vedlegg: VedleggBucketConfig,
                          val id: String,
-                         @DefaultValue val kms: KeyConfig) {
+                         @NestedConfigurationProperty @DefaultValue val kms: KeyConfig) {
+    @Value("\${spring.application.name}")
+    lateinit var name: String
     val kryptoKey = CryptoKeyName.of(id, LocationName.of(id, REGION).location, kms.ring, kms.nøkkel).toString()
 
     data class KeyConfig(val ring: String = "aap-mellomlagring-kms",
                          val nøkkel: String = "aap-mellomlagring-kms-key")
 
     data class MellomlagringBucketConfig(val navn: String,
-                                         @DefaultValue val subscription: SubscriptionConfig,
+                                         @NestedConfigurationProperty @DefaultValue val subscription: SubscriptionConfig,
                                          @DefaultValue(DEFAULT_TIMEOUT) val timeout: Duration) {
 
         data class SubscriptionConfig(val navn: String = "aap-mellomlagring-subscription",
