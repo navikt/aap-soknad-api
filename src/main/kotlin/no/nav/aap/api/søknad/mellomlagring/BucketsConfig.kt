f4395ac918754d670b0798454dd3e919f4633daf
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-01 13:01:29 +0200

    chore

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketsConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/BucketsConfig.kt
@@ -3,6 +3,7 @@ package no.nav.aap.api.søknad.mellomlagring
 import com.google.cloud.kms.v1.CryptoKeyName
 import com.google.cloud.kms.v1.KeyRingName
 import com.google.cloud.kms.v1.LocationName
+import com.google.pubsub.v1.ProjectSubscriptionName
 import no.nav.aap.api.søknad.mellomlagring.BucketsConfig.Companion.BUCKETS
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
@@ -18,6 +19,7 @@ data class BucketsConfig(val id: String,
                          @NestedConfigurationProperty private val kms: KeyConfig) {
 
     val timeoutMs = mellom.timeout.toMillis()
+    val projectSubscription = ProjectSubscriptionName.of(id, mellom.subscription.navn)
     val location = LocationName.of(id, REGION)
     val ring = KeyRingName.of(id, location.location, kms.ring)
     val ringNavn = ring.toString()
