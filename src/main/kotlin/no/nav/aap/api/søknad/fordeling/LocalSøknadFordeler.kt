7ce52de47eecb9921b7f53fd713c06d1b46e7f77
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-08 17:40:04 +0200

    lagt til pdfa sjekking

--- src/main/kotlin/no/nav/aap/api/søknad/fordeling/LocalSøknadFordeler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/fordeling/LocalSøknadFordeler.kt
@@ -3,13 +3,13 @@ package no.nav.aap.api.søknad.fordeling
 import no.nav.aap.api.søknad.model.Kvittering
 import no.nav.aap.api.søknad.model.StandardSøknad
 import no.nav.aap.api.søknad.model.UtlandSøknad
-import no.nav.aap.util.LoggerUtil
+import no.nav.aap.util.LoggerUtil.getLogger
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import java.util.*
 
 @ConditionalOnMissingBean(SøknadFordeler::class)
 class LocalSøknadFordeler : Fordeler {
-    private val log = LoggerUtil.getLogger(javaClass)
+    private val log = getLogger(javaClass)
     override fun fordel(søknad: UtlandSøknad) =
         Kvittering(UUID.randomUUID())
             .also {
---
+++ src/main/kotlin/no/nav/aap/api/søknad/joark/pdf/PDFASjekker.kt
@@ -0,0 +1,33 @@
+package no.nav.aap.api.søknad.joark.pdf
+
+import no.nav.aap.api.søknad.mellomlagring.dokument.DokumentInfo
+import no.nav.aap.api.søknad.mellomlagring.dokument.DokumentSjekker
+import no.nav.aap.util.LoggerUtil.getLogger
+import org.apache.pdfbox.preflight.parser.PreflightParser
+import org.apache.pdfbox.preflight.utils.ByteArrayDataSource
+import org.springframework.stereotype.Component
+import java.io.ByteArrayInputStream
+
+@Component
+class PDFASjekker : DokumentSjekker {
+    private val log = getLogger(javaClass)
+
+    override fun sjekk(dokument: DokumentInfo) = try {
+        val parser = PreflightParser(ByteArrayDataSource(ByteArrayInputStream(dokument.bytes)))
+        parser.parse()
+        parser.preflightDocument.use {
+            it.validate()
+            with(it.result) {
+                if (isValid) {
+                    log.info("PDF validering resultat OK")
+                }
+                else {
+                    log.warn("PDF validering resultat feilet med ${this.errorsList}")
+                }
+            }
+        }
+    }
+    catch (e: Exception) {
+        log.warn("Feil ved sjekking av $dokument", e)
+    }
+}
