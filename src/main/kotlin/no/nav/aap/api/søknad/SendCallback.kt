37402520a24c2a0253f039084016e6e915c1a68c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-17 09:25:18 +0200

    bruk boolean

--- src/main/kotlin/no/nav/aap/api/søknad/SendCallback.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/SendCallback.kt
@@ -30,7 +30,7 @@ class SendCallback<K, V>(private val msg: String) : KafkaSendCallback<K, V> {
 
     private fun log(fnr: String, recordMetadata: RecordMetadata?) {
         with(recordMetadata) {
-            log.info("Sendte $msg med nøkkel $fnr og offset ${this?.offset()} på ${this?.topic()}")
+            log.info("Sendte $msg for $fnr og offset ${this?.offset()} på ${this?.topic()}")
         }
     }
 
