f71030dbf6fc09c17f17ea981ccebfb37f26db84
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-19 09:28:33 +0200

    ingen ekstern notif by defult for msg

--- src/main/kotlin/no/nav/aap/api/søknad/brukernotifikasjoner/DittNavBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/brukernotifikasjoner/DittNavBeanConfig.kt
@@ -9,6 +9,7 @@ import no.nav.doknotifikasjon.schemas.DoknotifikasjonStatus
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,13 +18,8 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.KafkaHeaders.OFFSET
-import org.springframework.kafka.support.KafkaHeaders.RECEIVED_MESSAGE_KEY
-import org.springframework.kafka.support.KafkaHeaders.RECEIVED_PARTITION_ID
-import org.springframework.kafka.support.KafkaHeaders.RECEIVED_TOPIC
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS
-import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 
@@ -38,13 +34,15 @@ class DittNavBeanConfig {
             }))
 
     @Bean
-    fun notifikasjonListenerContainerFactory(properties: KafkaProperties) =
+    fun notifikasjonListenerContainerFactory(properties: KafkaProperties,
+                                             @Value("\${spring.application.name}") navn: String) =
         ConcurrentKafkaListenerContainerFactory<String, DoknotifikasjonStatus>().apply {
             consumerFactory =
                 DefaultKafkaConsumerFactory(properties.buildConsumerProperties().apply {
                     put(KEY_DESERIALIZER_CLASS, StringDeserializer::class.java)
                     put(VALUE_DESERIALIZER_CLASS, KafkaAvroDeserializer::class.java)
                     put(SPECIFIC_AVRO_READER_CONFIG, true)
+                    // setRecordFilterStrategy { f -> f.value().bestillerId == navn }
                 })
         }
 
@@ -54,13 +52,9 @@ class DittNavBeanConfig {
 
         @KafkaListener(topics = ["teamdokumenthandtering.aapen-dok-notifikasjon-status"],
                 containerFactory = "notifikasjonListenerContainerFactory")
-        fun listen(@Payload status: DoknotifikasjonStatus,
-                   @Header(RECEIVED_MESSAGE_KEY) key: String,
-                   @Header(RECEIVED_PARTITION_ID) partition: Int,
-                   @Header(OFFSET) offset: Int,
-                   @Header(RECEIVED_TOPIC) topic: String) {
+        fun listen(@Payload status: DoknotifikasjonStatus) {
             with(status) {
-                log.trace("Fikk notifikasjon $this med key $key fra partisjon $partition, offset $offset  på topic $topic")
+                log.trace("Fikk notifikasjon $this")
             }
         }
     }
