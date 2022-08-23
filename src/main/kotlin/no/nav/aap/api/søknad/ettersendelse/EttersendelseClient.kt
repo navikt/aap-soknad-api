fc90c6d93d7bff0df20aaa115a2c0588d6d29b89
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-23 12:52:27 +0200

    mangler

--- src/main/kotlin/no/nav/aap/api/søknad/ettersendelse/EttersendelseClient.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/ettersendelse/EttersendelseClient.kt
@@ -4,8 +4,10 @@ import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.søknad.fordeling.SøknadRepository
 import no.nav.aap.api.søknad.fordeling.SøknadRepository.Søknad
 import no.nav.aap.api.søknad.model.VedleggType
+import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
+@Component
 class EttersendelseClient(private val repo: SøknadRepository) {
     fun søknaderMedMangler(fnr: Fødselsnummer) =
         repo.getSøknadByFnr(fnr.fnr)?.filter { s ->
