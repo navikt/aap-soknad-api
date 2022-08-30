bbc706882d36c493124fe9ce677dfcdc0a56d052
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-08-30 13:48:26 +0200

    var for cluster

--- src/main/kotlin/no/nav/aap/api/søknad/model/SøkerInfo.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/model/SøkerInfo.kt
@@ -2,11 +2,10 @@ package no.nav.aap.api.søknad.model
 
 import no.nav.aap.api.oppslag.arbeid.Arbeidsforhold
 import no.nav.aap.api.oppslag.behandler.RegistrertBehandler
-import no.nav.aap.api.oppslag.konto.KontoWebClientAdapter.Konto
 import no.nav.aap.api.oppslag.krr.Kontaktinformasjon
 
 data class SøkerInfo(val søker: Søker?,
                      val behandlere: List<RegistrertBehandler>,
                      val arbeidsforhold: List<Arbeidsforhold>,
                      val kontaktinformasjon: Kontaktinformasjon?,
-                     val konto: Konto? = null)
+                     val konto: String? = null)
