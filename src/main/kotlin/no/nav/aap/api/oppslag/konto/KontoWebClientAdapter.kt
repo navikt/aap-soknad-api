c9f985d25de25595d783f4d7137f5f6658c3aa32
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-05 12:08:37 +0200

    empty on 404

--- src/main/kotlin/no/nav/aap/api/oppslag/konto/KontoWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/konto/KontoWebClientAdapter.kt
@@ -5,10 +5,12 @@ import no.nav.aap.api.oppslag.konto.KontoConfig.Companion.KONTO
 import no.nav.aap.rest.AbstractWebClientAdapter
 import no.nav.aap.util.AuthContext
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
 
 @Component
 class KontoWebClientAdapter(@Qualifier(KONTO) client: WebClient,
@@ -22,6 +24,8 @@ class KontoWebClientAdapter(@Qualifier(KONTO) client: WebClient,
                 .uri(cf::kontoUri)
                 .accept(APPLICATION_JSON)
                 .retrieve()
+                .onStatus({ httpStatus -> NOT_FOUND == httpStatus },
+                        { Mono.empty() })
                 .bodyToMono<Map<String, String>>()
                 .doOnSuccess {
                     log.trace("Kontoinformasjon er $it")
@@ -29,6 +33,7 @@ class KontoWebClientAdapter(@Qualifier(KONTO) client: WebClient,
                 .doOnError { t: Throwable ->
                     log.warn("Kontoinformasjon oppslag feilet", t)
                 }
+
                 .defaultIfEmpty(emptyMap())
                 .onErrorReturn(emptyMap())
                 .block()?.tilKontonummer()
