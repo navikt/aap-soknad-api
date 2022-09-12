65ae546717849764d211c2e6e2b22ef74a008e44
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-12 09:38:52 +0200

    extension siste søknad

--- src/main/kotlin/no/nav/aap/api/søknad/model/StandardEttersending.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/model/StandardEttersending.kt
@@ -1,9 +1,8 @@
 package no.nav.aap.api.søknad.model
 
 import java.util.*
-import javax.validation.constraints.NotEmpty
 
 data class StandardEttersending(val søknadId: UUID?,
                                 val ettersendteVedlegg: List<EttersendtVedlegg>) {
-    data class EttersendtVedlegg(val ettersending: Vedlegg, @NotEmpty val vedleggType: VedleggType)
+    data class EttersendtVedlegg(val ettersending: Vedlegg, val vedleggType: VedleggType)
 }
