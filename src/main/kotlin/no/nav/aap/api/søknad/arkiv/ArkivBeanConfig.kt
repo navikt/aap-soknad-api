7ce8253023a7c53dd2b7dff58f7de4f3cb30c9e6
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-13 10:46:24 +0200

    log

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivBeanConfig.kt
@@ -3,11 +3,10 @@ package no.nav.aap.api.søknad.arkiv
 import no.nav.aap.api.søknad.arkiv.ArkivConfig.Companion.CLIENT_CREDENTIALS_ARKIV
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.util.Constants.JOARK
-import no.nav.aap.util.LoggerUtil
 import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.aap.util.StringExtensions.asBearer
-import no.nav.boot.conditionals.EnvUtil
 import no.nav.boot.conditionals.EnvUtil.*
+import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.beans.factory.annotation.Qualifier
@@ -17,6 +16,7 @@ import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import java.lang.IllegalArgumentException
 
 @Configuration
 class ArkivBeanConfig {
@@ -36,13 +36,16 @@ class ArkivBeanConfig {
     fun arkivClientCredentialFilterFunction(cfgs: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
         ExchangeFilterFunction { req, next ->
             log.trace(CONFIDENTIAL, "Gjør token exchange for ${req.url()}")
-            next.exchange(ClientRequest.from(req).header(AUTHORIZATION, service.systemBearerToken(cfgs)).build())
+            next.exchange(ClientRequest.from(req).header(AUTHORIZATION, service.systemBearerToken(cfgs.registration[CLIENT_CREDENTIALS_ARKIV])).build())
         }
 
-    private fun OAuth2AccessTokenService.systemBearerToken(cfgs: ClientConfigurationProperties) =
-        getAccessToken(cfgs.registration[CLIENT_CREDENTIALS_ARKIV]).accessToken.asBearer().also {
-            log.trace(CONFIDENTIAL,"Token exchange for $CLIENT_CREDENTIALS_ARKIV  OK, token er $it")
-        }
+    private fun OAuth2AccessTokenService.systemBearerToken(properties: ClientProperties?) =
+        properties?.let {
+            getAccessToken(properties).accessToken.asBearer().also {
+                log.trace(CONFIDENTIAL,"Token exchange for $this  OK, token er $it")
+            }
+        } ?: throw IllegalArgumentException("Ingen konfigurasjon for $CLIENT_CREDENTIALS_ARKIV, sjekk konfigurasjonen")
+
 
     @Bean
     fun arkivHealthIndicator(adapter: ArkivWebClientAdapter) = object : AbstractPingableHealthIndicator(adapter) {}
