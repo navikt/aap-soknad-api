9c396a19a1dc91de6693e4fc35641953fdc4a9ff
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-13 10:33:29 +0200

    log

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivBeanConfig.kt
@@ -3,7 +3,11 @@ package no.nav.aap.api.søknad.arkiv
 import no.nav.aap.api.søknad.arkiv.ArkivConfig.Companion.CLIENT_CREDENTIALS_ARKIV
 import no.nav.aap.health.AbstractPingableHealthIndicator
 import no.nav.aap.util.Constants.JOARK
+import no.nav.aap.util.LoggerUtil
+import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.aap.util.StringExtensions.asBearer
+import no.nav.boot.conditionals.EnvUtil
+import no.nav.boot.conditionals.EnvUtil.*
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.springframework.beans.factory.annotation.Qualifier
@@ -16,13 +20,14 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class ArkivBeanConfig {
+
+    private val log = getLogger(javaClass)
+
     @Qualifier(JOARK)
     @Bean
     fun webClientArkiv(builder: WebClient.Builder, cfg: ArkivConfig, @Qualifier(JOARK) arkivClientCredentialFilterFunction: ExchangeFilterFunction) =
         builder
             .baseUrl("${cfg.baseUri}")
-           // .filter(temaFilterFunction())
-          //  .filter(correlatingFilterFunction())
             .filter(arkivClientCredentialFilterFunction)
             .build()
 
@@ -30,11 +35,14 @@ class ArkivBeanConfig {
     @Qualifier(JOARK)
     fun arkivClientCredentialFilterFunction(cfgs: ClientConfigurationProperties, service: OAuth2AccessTokenService) =
         ExchangeFilterFunction { req, next ->
+            log.trace(CONFIDENTIAL, "Gjør token exchange for ${req.url()}")
             next.exchange(ClientRequest.from(req).header(AUTHORIZATION, service.systemBearerToken(cfgs)).build())
         }
 
     private fun OAuth2AccessTokenService.systemBearerToken(cfgs: ClientConfigurationProperties) =
-        getAccessToken(cfgs.registration[CLIENT_CREDENTIALS_ARKIV]).accessToken.asBearer()
+        getAccessToken(cfgs.registration[CLIENT_CREDENTIALS_ARKIV]).accessToken.asBearer().also {
+            log.trace(CONFIDENTIAL,"Token exchange for $CLIENT_CREDENTIALS_ARKIV  OK, token er $it")
+        }
 
     @Bean
     fun arkivHealthIndicator(adapter: ArkivWebClientAdapter) = object : AbstractPingableHealthIndicator(adapter) {}
