05468c8b998c19859491a1baefef65b322f6dfc6
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-27 13:11:18 +0200

    kode  6 eller 7

--- src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLWebClientAdapter.kt
@@ -6,6 +6,10 @@ import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.Navn
 import no.nav.aap.api.felles.PostNummer
 import no.nav.aap.api.oppslag.graphql.AbstractGraphQLAdapter
+import no.nav.aap.api.oppslag.pdl.PDLBarn.PDLAdresseBeskyttelse
+import no.nav.aap.api.oppslag.pdl.PDLBarn.PDLAdresseBeskyttelse.FORTROLIG
+import no.nav.aap.api.oppslag.pdl.PDLBarn.PDLAdresseBeskyttelse.STRENGT_FORTROLIG
+import no.nav.aap.api.oppslag.pdl.PDLBarn.PDLAdresseBeskyttelse.STRENGT_FORTROLIG_UTLAND
 import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLBostedadresse.PDLVegadresse
 import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLForelderBarnRelasjon
 import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLFødsel
@@ -19,6 +23,7 @@ import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import java.time.Instant.now
 import java.time.LocalDate
 
 @Component
@@ -27,8 +32,7 @@ class PDLWebClientAdapter(
         @Qualifier(PDL_USER) client: WebClient,
         @Qualifier(PDL_SYSTEM) private val systemWebClient: GraphQLWebClient,
         cfg: PDLConfig,
-        private val ctx: AuthContext) :
-    AbstractGraphQLAdapter(client, cfg) {
+        private val ctx: AuthContext) : AbstractGraphQLAdapter(client, cfg) {
 
     fun søker(medBarn: Boolean = false) =
         with(ctx.getFnr()) {
@@ -37,6 +41,8 @@ class PDLWebClientAdapter(
             } ?: throw JwtTokenMissingException()
         }
 
+
+        else emptyList()
     private fun fødselsdatoFra(fødsel: Set<PDLFødsel>?) = fødselsdatoFra(fødsel?.firstOrNull())
 
     private fun fødselsdatoFra(fødsel: PDLFødsel?) = fødsel?.fødselsdato
@@ -50,6 +56,16 @@ class PDLWebClientAdapter(
             .also { log.trace(CONFIDENTIAL, "Søker er $it") }
     }
 
+    private fun barnFra(r: List<PDLForelderBarnRelasjon>, medBarn: Boolean) =
+        if (medBarn) {
+            r.map { b -> query<PDLBarn>(systemWebClient, BARN_QUERY, b.relatertPersonsIdent)
+            }.filterNotNull()
+                .filter(::umyndig)
+                .filterNot(::beskyttet)
+                .map { barn ->  Barn(navnFra(barn.navn), fødselsdatoFra(barn.fødselsdato)) }
+        }
+        else emptyList()
+
     private fun adresseFra(a: PDLVegadresse?) = a?.let {
         Adresse(a.adressenavn, a.husbokstav, a.husnummer, PostNummer(a.postnummer))
             .also { log.trace(CONFIDENTIAL, "Adresse er $it") }
@@ -60,23 +76,11 @@ class PDLWebClientAdapter(
     private fun navnFra(n: PDLNavn) = Navn(n.fornavn, n.mellomnavn, n.etternavn)
         .also { log.trace(CONFIDENTIAL, "Navn er $it") }
 
-    private fun barnFra(r: List<PDLForelderBarnRelasjon>, medBarn: Boolean): List<Barn?> =
-        if (medBarn) r.map { it ->
-            query<PDLBarn>(systemWebClient, BARN_QUERY, it.relatertPersonsIdent)
-                ?.let { barn ->
-                    val b = Barn(navnFra(barn.navn), fødselsdatoFra(barn.fødselsdato))
-                    b.fødseldato?.let {
-                        if (it.isBefore(LocalDate.now().minusYears(18))) {
-                            null// myndig
-                        }
-                        else b
-                    } ?: b
-                }
-                .also { b -> log.trace(CONFIDENTIAL, "Barn er $b") }
-        }
-        else emptyList()
+    fun umyndig(pdlBarn: PDLBarn) = fødselsdatoFra(pdlBarn.fødselsdato)?.isAfter(LocalDate.now().minusYears(18)) ?: true
+    fun beskyttet(pdlBarn: PDLBarn) = pdlBarn.adressebeskyttelse?.any { it in listOf(FORTROLIG, STRENGT_FORTROLIG_UTLAND,STRENGT_FORTROLIG) } == true
 
-    override fun toString() =
+
+override fun toString() =
         "${javaClass.simpleName} [webClient=$webClient,graphQLWebClient=$userWebClient,authContext=$ctx, cfg=$cfg]"
 
     companion object {
