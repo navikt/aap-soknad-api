03389006aff997d7a6cf2f11c3127065364e43f7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-03 14:34:44 +0200

    toJson

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/pdf/PDFGeneratorWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/pdf/PDFGeneratorWebClientAdapter.kt
@@ -11,6 +11,7 @@ import no.nav.aap.api.søknad.model.PDFKvittering
 import no.nav.aap.api.søknad.model.Søker
 import no.nav.aap.api.søknad.model.UtlandSøknad
 import no.nav.aap.rest.AbstractWebClientAdapter
+import no.nav.aap.util.StringExtensions.toJson
 import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType.APPLICATION_JSON
@@ -24,21 +25,20 @@ import java.time.LocalDate.now
 class PDFGeneratorWebClientAdapter(@Qualifier(PDF) client: WebClient,
                                    private val cf: PDFGeneratorConfig,
                                    private val mapper: ObjectMapper) : AbstractWebClientAdapter(client, cf) {
-    fun generate(søker: Søker, kvittering: PDFKvittering) = generate(cf.standardPath, StandardData(søker, kvittering))
-    fun generate(søker: Søker, søknad: UtlandSøknad) = generate(cf.utlandPath, UtlandData(søker, søknad))
+    fun generate(søker: Søker, kvittering: PDFKvittering) = generate(cf.standardPath, StandardData(søker, kvittering).toJson(mapper))
+    fun generate(søker: Søker, søknad: UtlandSøknad) = generate(cf.utlandPath, UtlandData(søker, søknad).toJson(mapper))
     private fun generate(path: String, data: Any) =
         webClient.post()
             .uri { it.path(path).build() }
             .contentType(APPLICATION_JSON)
-            .bodyValue(mapper.writeValueAsString(data))
+            .bodyValue(data)
             .retrieve()
             .bodyToMono<ByteArray>()
             .doOnError { t: Throwable ->
                 log.warn("PDF-generering mot $path feiler", t)
             }
             .doOnSuccess {
-                log.trace(CONFIDENTIAL,
-                        "Sendte json ${mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data)}")
+                log.trace(CONFIDENTIAL, "Sendte json $data")
                 log.info("PDF-generering OK")
             }
             .block() ?: throw IntegrationException("O bytes i retur fra pdfgen, pussig")
