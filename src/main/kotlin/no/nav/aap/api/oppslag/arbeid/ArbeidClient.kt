bc9fa2ffc04c8bcf86872513d2af6efcbc9b238a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-04 13:38:35 +0200

    delt opp søkerinfo i separate endepunkt

--- src/main/kotlin/no/nav/aap/api/oppslag/arbeid/ArbeidClient.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/arbeid/ArbeidClient.kt
@@ -1,11 +1,8 @@
 package no.nav.aap.api.oppslag.arbeid
 
-import no.nav.aap.api.oppslag.arbeid.ArbeidConfig.Companion.ARBEID
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 @Component
-@ConditionalOnProperty("${ARBEID}.enabled", havingValue = "true")
 class ArbeidClient(private val arbeid: ArbeidWebClientAdapter,
                    private val org: OrganisasjonWebClientAdapter) {
     fun arbeidInfo() =
