3d42088002412e1785e598931b987320dc81ef1a
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-14 12:51:18 +0200

    random UUID for oppgave

--- src/main/kotlin/no/nav/aap/api/søknad/fordeling/SøknadFullfører.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/fordeling/SøknadFullfører.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.api.søknad.fordeling
 
 import java.util.*
+import java.util.UUID.randomUUID
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.SkjemaType.STANDARD
 import no.nav.aap.api.felles.SkjemaType.STANDARD_ETTERSENDING
@@ -82,7 +83,7 @@ class SøknadFullfører(private val dokumentLager: Dokumentlager,
     private fun Søknad.oppdaterMinSide(fnr: Fødselsnummer, erKomplett: Boolean) =
         minside.opprettBeskjed(fnr, "Vi har mottatt din ${STANDARD.tittel.decap()}", eventId  = eventid).also {
             if (!erKomplett) {
-                minside.opprettOppgave(fnr, "Vi har mottatt din ${STANDARD.tittel.decap()}. Du må ettersende dokumentasjon", eventid)
+                minside.opprettOppgave(fnr, "Vi har mottatt din ${STANDARD.tittel.decap()}. Du må ettersende dokumentasjon", randomUUID())
             }
         }
 
