f25441bb094d5ef4510d349f30d1f18c614f5a63
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-17 22:16:17 +0200

    omit for now

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.api.søknad.mellomlagring
 
 import java.util.*
+import java.util.concurrent.TimeUnit.SECONDS
 import no.nav.aap.api.felles.SkjemaType.STANDARD
 import no.nav.aap.api.søknad.minside.MinSideClient
 import no.nav.aap.api.søknad.minside.MinSideNotifikasjonType.Companion.MINAAPSTD
@@ -12,10 +13,10 @@ import org.springframework.stereotype.Component
 class MellomlagringVarsler(private val minside: MinSideClient, private val lager: Mellomlager, val cfg: BucketConfig) {
     val log = getLogger(javaClass)
 
-    @Scheduled(fixedDelayString = "#{'\${buckets.mellom.purring.delay}'}", initialDelay = 1000)
+    @Scheduled(fixedDelayString = "#{'\${buckets.mellom.purring.delay}'}", initialDelay = 10, timeUnit = SECONDS)
     fun scheduleFixedRateWithInitialDelayTask() {
         with(cfg.mellom.purring) {
-            log.trace("Orphan sjekk på $alder")
+            log.trace("Orphan sjekk på mellomlagringer eldre enn $alder")
             val gamle = lager.ikkeOppdatertSiden(alder)
             log.trace("${alder.toHoursPart()} timer skal varsles: $gamle")
             if (enabled) {
