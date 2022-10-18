e4000e7b3245887288a6ee36acb2eae2424654b3
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-18 09:16:54 +0200

    leader election

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
@@ -1,25 +1,30 @@
 package no.nav.aap.api.søknad.mellomlagring
 
+import java.net.InetAddress
+import java.net.URI
 import java.util.*
 import java.util.concurrent.TimeUnit.SECONDS
 import no.nav.aap.api.felles.SkjemaType.STANDARD
 import no.nav.aap.api.søknad.minside.MinSideClient
 import no.nav.aap.api.søknad.minside.MinSideNotifikasjonType.Companion.MINAAPSTD
 import no.nav.aap.util.LoggerUtil.getLogger
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType.*
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient.Builder
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Component
-class MellomlagringVarsler(private val minside: MinSideClient, private val lager: Mellomlager) {
+class MellomlagringVarsler(private val minside: MinSideClient, private val lager: Mellomlager, private val elector: LeaderElector) {
     val log = getLogger(javaClass)
-
     @Scheduled(fixedDelayString = "#{'\${buckets.mellom.purring.delay}'}", initialDelay = 10, timeUnit = SECONDS)
     fun scheduleFixedRateWithInitialDelayTask() {
         with(lager.config().purring) {
-            log.trace("Orphan sjekk på mellomlagringer eldre enn $alder")
+            log.trace("Orphan sjekk på mellomlagringer eldre enn $alder og leader status ${elector.isLeaader()}")
             val gamle = lager.ikkeOppdatertSiden(alder)
             log.trace("${alder.toHoursPart()} timer skal varsles: $gamle")
-            if (enabled) {
+            if (enabled  && elector.isLeaader()) {
                 gamle.forEach {
                     log.trace("Avslutter ${it.third} for ${it.first} siden opprettet er ${it.second}")
                     minside.avsluttBeskjed(STANDARD,it.first,it.third)
@@ -29,3 +34,27 @@ class MellomlagringVarsler(private val minside: MinSideClient, private val lager
         }
     }
 }
+
+
+    @Component
+    class LeaderElector(@Value("\${selector.path}") private val selector: URI, private val b: Builder) {
+        val log = getLogger(javaClass)
+
+        fun isLeaader() =
+            with(selector) {
+                b.baseUrl("http:// $host:$port").build()
+                    .get()
+                    .accept(APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono<Leader>()
+                    .doOnError { t: Throwable ->
+                        log.warn("Leader oppslag feilet", t)
+                    }
+                    .doOnSuccess {
+                        log.trace("Leader er $it")
+                    }
+                    .block()?.name == (InetAddress.getLocalHost().hostName
+                    ?: throw IllegalStateException("Kunne ikke slå opp leader"))
+    }
+        data class Leader(val name: String)
+    }
