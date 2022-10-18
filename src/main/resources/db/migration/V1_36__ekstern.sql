b5ae555a1e4c2a01a1f9216200f7df351ae482d9
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-18 20:30:10 +0200

    add column ekstern

--- src/main/resources/db/migration/V1_36__ekstern.sql
+++ src/main/resources/db/migration/V1_36__ekstern.sql
@@ -2,7 +2,3 @@ ALTER TABLE minsidebeskjeder
     ADD COLUMN ekstern BOOLEAN;
 ALTER TABLE minsideoppgaver
     ADD COLUMN ekstern BOOLEAN;
-UPDATE minsidebeskjeder set ekstern = FALSE;
-UPDATE minsideoppgaver set ekstern = FALSE;
-ALTER TABLE minsidebeskjeder ALTER COLUMN ekstern SET NOT NULL;
-ALTER TABLE minsideoppgaver ALTER COLUMN ekstern SET NOT NULL;
