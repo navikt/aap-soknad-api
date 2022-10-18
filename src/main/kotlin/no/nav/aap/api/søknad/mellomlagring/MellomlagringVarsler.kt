71098341d9642baf59d9615c0728351b50659cf7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-10-18 15:34:52 +0200

    leader election

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringVarsler.kt
@@ -22,15 +22,20 @@ class MellomlagringVarsler(private val minside: MinSideClient, private val lager
     @Scheduled(fixedDelayString = "#{'\${buckets.mellom.purring.delay}'}", initialDelay = 10, timeUnit = SECONDS)
     fun sjekkVarsling() {
         with(lager.config().purring) {
-            log.trace("Orphan sjekk på mellomlagringer eldre enn $alder og leader status ${elector.isLeaader()}")
-            val gamle = lager.ikkeOppdatertSiden(alder)
-            log.trace("${alder.toHoursPart()} timer skal varsles: $gamle")
-            if (enabled  && elector.isLeaader()) {
-                gamle.forEach {
-                    log.trace("Avslutter ${it.third} for ${it.first} siden opprettet er ${it.second}")
-                    minside.avsluttBeskjed(STANDARD,it.first,it.third)
-                    minside.opprettBeskjed(it.first,"Dette er en purring", UUID.randomUUID(), MINAAPSTD,true)
+            if (enabled)  {
+                log.trace("Ser etter snart utgåtte mellomlagringer eldre enn ${alder.toHours()} og leader status ${elector.isLeaader()}")
+                val gamle = lager.ikkeOppdatertSiden(alder)
+                log.trace("Disse skal varsles:  $gamle")
+                if (elector.isLeaader()) {
+                    gamle.forEach {
+                        log.trace("Avslutter ${it.third} for ${it.first} siden opprettet er ${it.second}")
+                        minside.avsluttBeskjed(STANDARD,it.first,it.third)
+                        minside.opprettBeskjed(it.first,"Dette er en purring", UUID.randomUUID(), MINAAPSTD,true)
+                    }
                 }
+            }
+            else {
+                log.trace("Ingen sjekk av snart utgåtte mellomlagringer")
             }
         }
     }
@@ -42,19 +47,19 @@ class LeaderElector(@Value("\${elector.path}") private val elector: String, priv
     val log = getLogger(javaClass)
 
     fun isLeaader() =
-            b.baseUrl("http://$elector").build()
-                .get()
-                .accept(APPLICATION_JSON, parseMediaType("text/plain; charset=utf-8"))
-                .retrieve()
-                .bodyToMono<Leader>()
-                .doOnError { t: Throwable ->
-                    log.warn("Leader oppslag feilet", t)
-                }
-                .doOnSuccess {
-                    log.trace("Leader er $it")
-                }
-                .block()?.name == (InetAddress.getLocalHost().hostName
-                ?: throw IllegalStateException("Kunne ikke slå opp leader"))
+        b.baseUrl("http://$elector").build()
+            .get()
+            .accept(APPLICATION_JSON, parseMediaType("text/plain; charset=utf-8"))
+            .retrieve()
+            .bodyToMono<Leader>()
+            .doOnError { t: Throwable ->
+                log.warn("Leader oppslag feilet", t)
+            }
+            .doOnSuccess {
+                log.trace("Leader er $it")
+            }
+            .block()?.name == (InetAddress.getLocalHost().hostName
+            ?: false)
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     data class Leader(val name: String)
