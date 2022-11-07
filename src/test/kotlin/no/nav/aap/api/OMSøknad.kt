5335e028beddd5538339ddc9268683bd19ce53cd
Niro Karunakaran <niro.karunakaran@nav.no>
2022-11-07 13:44:01 +0100

    :white_check_mark: la til er_student_søknad som utløser krav om dokumentasjon

--- src/test/kotlin/no/nav/aap/api/OMSøknad.kt
+++ src/test/kotlin/no/nav/aap/api/OMSøknad.kt
@@ -52,5 +52,49 @@ class OMSøknad {
                     UUID.randomUUID()))
             )
         }
+
+        fun er_student_søknad(): StandardSøknad{
+            return StandardSøknad(
+                false,
+                null,
+                Studier(Studier.StudieSvar.AVBRUTT, RadioValg.JA),
+                Medlemskap(true, null, null, null,
+                    listOf(
+                        Utenlandsopphold(
+                            CountryCode.SE,
+                            Periode(LocalDate.now(), LocalDate.now().plusDays(2)),
+                            true, "11111111")
+                    )),
+                listOf(
+                    RegistrertBehandler(
+                        RegistrertBehandler.BehandlerType.FASTLEGE, RegistrertBehandler.BehandlerKategori.LEGE, Navn("Lege", "A", "Legesen"),
+                        RegistrertBehandler.KontaktInformasjon("Legekontoret",
+                            OrgNummer("888888888"),
+                            Adresse("Legegata", "17", "A",
+                                PostNummer("2600", "Lillehammer")
+                            ),
+                            "22222222")
+                    )
+                ), emptyList(),
+                StandardSøknad.Yrkesskade.JA,
+                Utbetalinger(
+                    Utbetalinger.FraArbeidsgiver(true, Vedlegg(deler = listOf(
+                        UUID.randomUUID(),
+                        UUID.randomUUID()))
+                    ), listOf(Utbetalinger.AnnenStønad(Utbetalinger.AnnenStønadstype.INTRODUKSJONSSTØNAD))),
+                "Tillegg",
+                listOf(BarnOgInntekt(true)),
+                listOf(
+                    AnnetBarnOgInntekt(
+                        Søker.Barn(Navn("Et", "ekstra", "Barn"), LocalDate.now().minusYears(14)),
+                        AnnetBarnOgInntekt.Relasjon.FORELDER,false,
+                        Vedlegg(deler = listOf(UUID.randomUUID()))
+                    )
+                ), Vedlegg(deler = listOf(
+                    UUID.randomUUID(),
+                    UUID.randomUUID()))
+            )
+        }
+
     }
 }
