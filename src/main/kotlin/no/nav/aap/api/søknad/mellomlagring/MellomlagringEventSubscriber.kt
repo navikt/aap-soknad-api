2949099affa9da996280f60c54462a5c486e67e7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-08 15:24:47 +0100

    ikke opprett melding om mellomlagring foreløpig

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
@@ -47,7 +47,7 @@ class MellomlagringEventSubscriber(private val dittNav: MinSideClient,
                         log.info("PubSub event $type med metadata $it")
                         when (type) {
                             OBJECT_FINALIZE -> Unit //opprettet(it) TODO vi dropper denne foreløpig
-                            OBJECT_DELETE -> Unit // slettet(it)
+                            OBJECT_DELETE -> Unit // slettet(it)  // TODO også denne
                             else -> log.warn("Event $type ikke håndtert (dette skal aldri skje)")
                         }
                     } ?: log.warn("Fant ikke forventede metadata i event ${event.pubsubMessage}")
