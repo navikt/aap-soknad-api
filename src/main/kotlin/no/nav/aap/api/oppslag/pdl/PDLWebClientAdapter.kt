90f21245ea46ba166ddd1cfe8cc49c10f9ba7dbe
vetleh <vetle.hollund@nav.no>
2022-11-09 09:55:52 +0100

    Trukket ut mapping til eget object fra PDL

--- src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/pdl/PDLWebClientAdapter.kt
@@ -1,24 +1,12 @@
 package no.nav.aap.api.oppslag.pdl
 
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient
-import java.time.LocalDate
-import no.nav.aap.api.felles.Adresse
-import no.nav.aap.api.felles.Fødselsnummer
-import no.nav.aap.api.felles.Navn
-import no.nav.aap.api.felles.PostNummer
 import no.nav.aap.api.oppslag.graphql.AbstractGraphQLAdapter
-import no.nav.aap.api.oppslag.pdl.PDLAdresseBeskyttelse.FORTROLIG
-import no.nav.aap.api.oppslag.pdl.PDLAdresseBeskyttelse.STRENGT_FORTROLIG
-import no.nav.aap.api.oppslag.pdl.PDLAdresseBeskyttelse.STRENGT_FORTROLIG_UTLAND
-import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLBostedadresse.PDLVegadresse
 import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLForelderBarnRelasjon
-import no.nav.aap.api.oppslag.pdl.PDLSøker.PDLFødsel
-import no.nav.aap.api.søknad.model.Søker
-import no.nav.aap.api.søknad.model.Søker.Barn
+import no.nav.aap.api.oppslag.pdl.PDLmapper.pdlSøkerTilSøker
 import no.nav.aap.util.AuthContext
 import no.nav.aap.util.Constants.PDL_SYSTEM
 import no.nav.aap.util.Constants.PDL_USER
-import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
 import no.nav.security.token.support.core.exceptions.JwtTokenMissingException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType.APPLICATION_JSON
@@ -36,7 +24,6 @@ data class WebClients(
 class PDLWebClientAdapter(private val clients: WebClients, cfg: PDLConfig, private val ctx: AuthContext) :
     AbstractGraphQLAdapter(clients.client, cfg) {
 
-
     override fun ping() :Map<String,String>{
         webClient
             .options()
@@ -51,68 +38,13 @@ class PDLWebClientAdapter(private val clients: WebClients, cfg: PDLConfig, priva
     fun søker(medBarn: Boolean = false) =
         with(ctx.getFnr()) {
             query<PDLWrappedSøker>(clients.user, PERSON_QUERY, fnr)?.active?.let {
-                søkerFra(it, this, medBarn)
+                pdlSøkerTilSøker(it, this, barn(medBarn,it.forelderBarnRelasjon))
             } ?: throw JwtTokenMissingException()
         }
 
-    private fun fødselsdatoFra(fødsel: Set<PDLFødsel>?) = fødselsdatoFra(fødsel?.firstOrNull())
-
-    private fun fødselsdatoFra(fødsel: PDLFødsel?) = fødsel?.fødselsdato
-
-    private fun søkerFra(søker: PDLSøker?, fnr: Fødselsnummer, medBarn: Boolean) = søker?.let {
-        with(it) {
-            Søker(navnFra(navn), fnr,
-                    søker.beskyttet(),
-                    if (!it.beskyttet()) {
-                        adresseFra(vegadresse)
-                    } else null,
-                    fødselsdatoFra(fødsel),
-                    barnFra(forelderBarnRelasjon, medBarn))
-                .also {
-                    log.trace(CONFIDENTIAL, "Søker er $it")
-                }
-        }
-    }
-
-    private fun barnFra(r: List<PDLForelderBarnRelasjon>, medBarn: Boolean) =
-        if (medBarn) {
-            r.asSequence().map {
-                query<PDLBarn>(clients.system, BARN_QUERY, it.relatertPersonsIdent)
-            }.filterNotNull()
-                .filterNot(::myndig)
-                .filterNot(::beskyttet)
-                .filterNot(::død)
-                .map { Barn(navnFra(it.navn), fødselsdatoFra(it.fødselsdato)) }.toList()
-        }
-        else emptyList()
-
-    private fun adresseFra(adresse: PDLVegadresse?) = adresse?.let {
-        with(it) {
-                Adresse(adressenavn, husbokstav, husnummer, PostNummer(postnummer))
-        }
-    }
-
-    private fun navnFra(navn: Set<PDLNavn>) = navnFra(navn.first())
-
-    private fun navnFra(navn: PDLNavn) =
-        with(navn) {
-            Navn(fornavn, mellomnavn, etternavn)
-                .also { log.trace(CONFIDENTIAL, "Navn er $it") }
-        }
-
-    private fun myndig(pdlBarn: PDLBarn) =
-        fødselsdatoFra(pdlBarn.fødselsdato)?.isBefore(LocalDate.now().minusYears(18)) ?: true
-
-    private fun beskyttet(pdlBarn: PDLBarn) = beskyttet(pdlBarn.adressebeskyttelse)
-
-    private fun PDLSøker.beskyttet() = beskyttet(adressebeskyttelse)
-
-
-    private fun beskyttet(gradering: Set<PDLGradering>?) = gradering?.any {
-        it.gradering in listOf(FORTROLIG, STRENGT_FORTROLIG_UTLAND, STRENGT_FORTROLIG)
-    } == true
-
-    private fun død(pdlBarn: PDLBarn) = pdlBarn.dødsfall?.any() ?: false
+    fun barn(medBarn:Boolean,forelderBarnRelasjon: List<PDLForelderBarnRelasjon>) = if(medBarn) { forelderBarnRelasjon.asSequence().map {
+        query<PDLBarn>(clients.system, BARN_QUERY, it.relatertPersonsIdent) }.filterNotNull()
+    } else emptySequence()
 
     override fun toString() =
         "${javaClass.simpleName} [webClient=$webClient,webClients=$clients,authContext=$ctx, cfg=$cfg]"
