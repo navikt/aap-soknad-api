d1055aedd3d29243a8a8f9663a99d44f814bb5d8
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-10 10:00:03 +0100

    log første 5 siffer av fnr

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/StørelseSjekker.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/StørelseSjekker.kt
@@ -6,19 +6,20 @@ import com.google.cloud.storage.Storage.BlobListOption.prefix
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.søknad.mellomlagring.BucketConfig.VedleggBucketConfig
 import no.nav.aap.api.søknad.mellomlagring.dokument.DokumentInfo
+import no.nav.aap.util.AuthContext
 import no.nav.aap.util.LoggerUtil
 import org.springframework.stereotype.Component
 import org.springframework.util.unit.DataSize
 
 @Component
-class StørelseSjekker(private val lager: Storage) {
+class StørelseSjekker(private val lager: Storage, private val ctx: AuthContext) {
 
     val log = LoggerUtil.getLogger(javaClass)
     fun sjekkStørrelse(cfg: VedleggBucketConfig, fnr: Fødselsnummer, dokument: DokumentInfo) =
         with(cfg) {
             val sum = lager.list(navn, prefix("${fnr.fnr}/"), currentDirectory()).iterateAll().sumOf { it.size }
             if (sum + dokument.size > maxsum.toBytes()) {
-                log.warn("Opplasting av dokument med størrelse  ${DataSize.ofBytes(dokument.size)} burde ikke tillates, har allerede lastet opp ${DataSize.ofBytes(sum)}, max pr bruker er er $maxsum")
+                log.warn("(${ctx.getFnr()})  Opplasting av dokument med størrelse  ${DataSize.ofBytes(dokument.size)} burde ikke tillates, har allerede lastet opp ${DataSize.ofBytes(sum)}, max pr bruker er er $maxsum")
             }
             else {
                 log.trace("Opplasting av dokument med størrelse  ${DataSize.ofBytes(dokument.size)} tillates, samled størrelse av vedlegg i bøtte er ${DataSize.ofBytes(sum)}")
