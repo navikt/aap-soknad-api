4791f3b249cd819152c6205376a04aa26d46778b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-18 21:31:09 +0100

    chore

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
@@ -6,13 +6,12 @@ import no.nav.boot.conditionals.ConditionalOnGCP
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
-import org.springframework.stereotype.Component
 
 @ConditionalOnGCP
 class ArkivHendelseKonsument {
         private val log = getLogger(javaClass)
 
-        @KafkaListener(topics = ["#{'\${joark.hendelser.topic}'}"], groupId = "#{'\${joark.hendelser.groupid}'}", containerFactory = ARKIVHENDELSER)
+        @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = ARKIVHENDELSER)
         fun listen(@Payload hendelse: JournalfoeringHendelseRecord)  {
                 log.info("Mottatt arkivføringshendelse $hendelse")
         }
