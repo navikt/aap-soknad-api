8d35df6c031c89f1c178c59ee7709e35e0ddea1e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-21 16:45:06 +0100

    bump

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
@@ -1,13 +1,10 @@
 package no.nav.aap.api.søknad.arkiv
 
-import java.time.LocalDateTime
 import java.time.LocalDateTime.parse
-import java.time.ZoneId.of
-import java.time.ZoneId.systemDefault
-import java.time.ZoneOffset.UTC
 import no.nav.aap.api.søknad.arkiv.ArkivConfig.Companion.ARKIVHENDELSER
 import no.nav.aap.api.søknad.fordeling.SøknadRepository
 import no.nav.aap.util.LoggerUtil.getLogger
+import no.nav.aap.util.TimeExtensions.toUTC
 import no.nav.boot.conditionals.ConditionalOnGCP
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -21,13 +18,10 @@ class ArkivHendelseKonsument(private val repo: SøknadRepository) {
       @Transactional
         @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = ARKIVHENDELSER)
         fun listen(@Payload hendelse: JournalfoeringHendelseRecord)  {
-                log.info("Mottatt arkivføringshendelse $hendelse ${systemDefault()}")
                 repo.getSøknadByJournalpostid("${hendelse.journalpostId}")?.let {
-                        log.info("Fant opprinnelig søknad $it for arkivføringshendelse $hendelse")
-                        it.journalfoert = hendelse.tilTid()
+                        it.journalfoert = hendelse.tilUTC()
                 }
         }
 
-    private fun JournalfoeringHendelseRecord.tilTid()  = parse(hendelsesId.substringAfter('-')).toUTC()
-    private fun LocalDateTime.toUTC(): LocalDateTime = atZone(of("Europe/Oslo")).withZoneSameInstant(UTC).toLocalDateTime()
+    private fun JournalfoeringHendelseRecord.tilUTC()  = parse(hendelsesId.substringAfter('-')).toUTC()
 }
