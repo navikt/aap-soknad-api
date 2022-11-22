1bc63a2fc2644af8c4b81e6797991a737ed2cb3d
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-11-22 09:58:12 +0100

    pingable kafka

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivConfig.kt
@@ -5,6 +5,7 @@ import no.nav.aap.rest.AbstractRestConfig
 import no.nav.aap.util.Constants.JOARK
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.boot.context.properties.bind.DefaultValue
 
 @ConfigurationProperties(JOARK)
@@ -13,7 +14,10 @@ class ArkivConfig(
         @DefaultValue(DEFAULT_OPPRETT_PATH) val arkivPath: String,
         @DefaultValue(DEFAULT_PING_PATH) pingPath: String,
         @DefaultValue("true") enabled: Boolean,
+        @NestedConfigurationProperty val hendelser: HendelseConfig,
         baseUri: URI) : AbstractRestConfig(baseUri, pingPath, JOARK, enabled) {
+
+    data class HendelseConfig(val topic: String)
 
     override fun toString() = "${javaClass.simpleName} [pingPath=$pingPath,arkivPath=$arkivPath,enabled=$isEnabled,baseUri=$baseUri]"
 
