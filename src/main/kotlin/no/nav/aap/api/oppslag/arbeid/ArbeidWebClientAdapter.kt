fe4378b9a49eaf7c4bc1efa40d486304fb6dc3dc
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-12-04 15:06:27 +0100

    bump

--- src/main/kotlin/no/nav/aap/api/oppslag/arbeid/ArbeidWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/arbeid/ArbeidWebClientAdapter.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException.*
 import org.springframework.web.reactive.function.client.bodyToMono
 
 @Component
