ff26a73a13f25eee9baccfe9f865c39e23b2db74
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-12-04 10:44:29 +0100

    test webclient

--- src/main/kotlin/no/nav/aap/api/oppslag/behandler/BehandlerConfig.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/behandler/BehandlerConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.boot.context.properties.bind.DefaultValue
+import org.springframework.boot.convert.DurationStyle.*
 import org.springframework.web.util.UriBuilder
 import reactor.util.retry.Retry.*
 
@@ -18,14 +19,21 @@ class BehandlerConfig(
         @DefaultValue(DEFAULT_URI) baseUri: URI,
         @DefaultValue(DEFAULT_PING_PATH) pingPath: String = DEFAULT_PING_PATH,
         @DefaultValue(DEFAULT_PATH) private val path: String = DEFAULT_PATH,
-        @NestedConfigurationProperty val retry: RetryConfig = RetryConfig(3, ofMillis(100)),
+        @NestedConfigurationProperty val retry: RetryConfig = DEFAULT_RETRY,
         @DefaultValue("true") enabled: Boolean = true) : AbstractRestConfig(baseUri, pingPath, BEHANDLER, enabled,retry.retries,retry.delayed) {
     fun path(b: UriBuilder) = b.path(path).build()
 
-    data class RetryConfig(@DefaultValue("3")  val retries: Long,
-                           @DefaultValue("100ms")  val delayed: Duration)
+    data class RetryConfig(@DefaultValue(DEFAULT_RETRIES)  val retries: Long,
+                           @DefaultValue(DEFAULT_DELAY)  val delayed: Duration) {
+        companion object {
+            const val DEFAULT_RETRIES = "3"
+            const val DEFAULT_DELAY = "133ms"
+            val DEFAULT = RetryConfig(DEFAULT_RETRIES.toLong(), detectAndParse(DEFAULT_DELAY))
+        }
+    }
 
     companion object {
+        val DEFAULT_RETRY = RetryConfig.DEFAULT
         const val BEHANDLER = "behandler"
         const val BEHANDLERPING = "${BEHANDLER}ping"
         const val DEFAULT_URI = "http://isdialogmelding.teamsykefravr"
