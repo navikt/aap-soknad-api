3b0df3c8d08e5704d0f5bd3c44d0ad2033f3122e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-12-06 21:26:38 +0100

    endelig fikset testen

--- src/test/kotlin/no/nav/aap/api/søknad/arkiv/ArkivTest.kt
+++ src/test/kotlin/no/nav/aap/api/søknad/arkiv/ArkivTest.kt
@@ -1,10 +1,10 @@
 package no.nav.aap.api.søknad.arkiv
 
 import java.util.*
-import kotlin.test.assertNotNull
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.api.felles.MockWebServerExtensions.expect
 import no.nav.aap.api.felles.Navn
+import no.nav.aap.api.søknad.arkiv.ArkivClient.ArkivResultat
 import no.nav.aap.api.søknad.arkiv.Journalpost.AvsenderMottaker
 import no.nav.aap.api.søknad.arkiv.Journalpost.Bruker
 import no.nav.aap.api.søknad.arkiv.Journalpost.Dokument
@@ -49,16 +49,12 @@ class ArkivTest {
     @Test
     fun ok() {
         arkiv.expect(kvittering,CREATED)
-        var r = client.arkiver(journalpost())
-        assertNotNull(r)
-        assertThat(r.journalpostId).isEqualTo("42")
+        assertOK(client.arkiver(journalpost()))
     }
     @Test
     fun conflict() {
         arkiv.expect(kvittering,CONFLICT)
-        var r = client.arkiver(journalpost())
-        assertNotNull(r)
-        assertThat(r.journalpostId).isEqualTo("42")
+        assertOK(client.arkiver(journalpost()))
     }
         @Test
         fun bad() {
@@ -70,11 +66,14 @@ class ArkivTest {
     @Test
     fun opprettetOKMenResponsenKomAldriTilbake() {
         arkiv.expect(BAD_GATEWAY).expect(kvittering,CONFLICT)
-        var r = client.arkiver(journalpost())
-        assertNotNull(r)
-        assertThat(r.journalpostId).isEqualTo("42")
+        assertOK(client.arkiver(journalpost()))
     }
 
+    private fun assertOK(resultat: ArkivResultat) {
+        assertThat(resultat).isNull()
+        assertThat(resultat.journalpostId).isEqualTo("42")
+
+    }
     private fun journalpost() = Journalpost("tittel",
             AvsenderMottaker(Fødselsnummer("08089403198"),
             Navn("Test","Tester","Testsen")),
