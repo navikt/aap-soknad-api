a68a709f1a3a9ea4a55f414b1f35772c18bcdfa5
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-01-06 15:57:40 +0100

    bolk oppslag barn med fallback

--- src/main/kotlin/no/nav/aap/api/oppslag/person/PDLWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/person/PDLWebClientAdapter.kt
@@ -56,11 +56,10 @@ class PDLWebClientAdapter(private val clients: WebClients, cfg: PDLConfig, priva
                 val barnIDer  = forelderBarnRelasjon.mapNotNull { it.relatertPersonsIdent }
                 if (barnIDer.isNotEmpty()) {
                     log.trace("Slår opp barn $barnIDer")
-                    queryBolk<Map<String,Any>>(clients.system, BARN_BOLK_QUERY,barnIDer)?.asSequence() ?: emptyList<PDLBarn>().asSequence()
-                        .also {
-                        log.trace("Slo opp barn $it")
-                            throw IllegalStateException("OOPS")
-                    }
+                    val b = queryBolk<Map<String,Any>>(clients.system, BARN_BOLK_QUERY,barnIDer)?.asSequence() ?: emptyList<PDLBarn>().asSequence()
+                    log.trace("Slo opp barn $b")
+                    throw IllegalStateException("OOPS")
+
                 }
                 else {
                     emptyList<PDLBarn>().asSequence()
