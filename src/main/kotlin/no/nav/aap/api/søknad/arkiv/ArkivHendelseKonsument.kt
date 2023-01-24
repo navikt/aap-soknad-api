b22afe4ffce653db73de6e889075d78e2b3db94c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-01-24 20:53:36 +0100

    log

--- src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/arkiv/ArkivHendelseKonsument.kt
@@ -18,14 +18,15 @@ class ArkivHendelseKonsument(private val repo: SøknadRepository) {
     @Transactional
     @KafkaListener(topics = ["#{'\${joark.hendelser.topic:teamdokumenthandtering.aapen-dok-journalfoering}'}"], containerFactory = ARKIVHENDELSER)
     fun listen(@Payload hendelse: JournalfoeringHendelseRecord)  {
+        repo.getSøknadByEttersendingJournalpostid("${hendelse.journalpostId}")?.let {
+            log.info("Søknad via ettersending journalpost er $it")
+            log.info("Søknad ettersending er ${it.ettersendinger}")
+        } ?: log.info("Ikke søknad via ettersending")
         repo.getSøknadByJournalpostid("${hendelse.journalpostId}")?.let {
             it.journalpoststatus = hendelse.journalpostStatus
             it.journalfoert = hendelse.tilUTC()
-            log.info("Søknad direkte er $it")
-        }   ?: repo.getSøknadByEttersendingJournalpostid("${hendelse.journalpostId}")?.let {
-               log.info("Søknad via ettersending journalpost er $it")
-               log.info("Søknad ettersending er ${it.ettersendinger}")
-        } ?: log.info("Ingen søknad eller ettersendelse for journalpost ${hendelse.journalpostId} funnet for hendelse $hendelse")
+            log.info("Søknad direkte for ${hendelse.journalpostStatus} er $it")
+        }  ?: log.info("Ingen søknad for journalpost ${hendelse.journalpostId} funnet")
     }
 
     private fun JournalfoeringHendelseRecord.tilUTC()  = parse(hendelsesId.substringAfter('-')).toUTC()
