e51ff5d10e3be4b92e6ba657ec81eb58644285a9
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-10 08:59:07 +0100

    aap

--- src/main/kotlin/no/nav/aap/api/søknad/minside/MinSideForside.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/minside/MinSideForside.kt
@@ -2,6 +2,7 @@ package no.nav.aap.api.søknad.minside
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.aap.api.felles.Fødselsnummer
+import no.nav.aap.util.Constants.AAP
 
 data class MinSideForside(@JsonProperty("@event_name")  val eventName: EventName, val ident: Fødselsnummer, val microfrontend_id: String = MICROFRONTEND_ID) {
 
@@ -9,6 +10,6 @@ data class MinSideForside(@JsonProperty("@event_name")  val eventName: EventName
         enable,disable
     }
     companion object {
-        private const val MICROFRONTEND_ID = "aap"
+        private const val MICROFRONTEND_ID = AAP
     }
 }
