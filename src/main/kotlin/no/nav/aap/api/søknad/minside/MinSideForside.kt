89c97f6ab0a035a60588d102e7609f81cd7b21f7
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-20 13:37:35 +0100

    action it is

--- src/main/kotlin/no/nav/aap/api/søknad/minside/MinSideForside.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/minside/MinSideForside.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.util.Constants.AAP
 
-data class MinSideForside(@JsonProperty("@event_name")  val eventName: EventName, val ident: Fødselsnummer, val microfrontend_id: String = MICROFRONTEND_ID) {
+data class MinSideForside(@JsonProperty("@action")  val eventName: EventName, val ident: Fødselsnummer, val microfrontend_id: String = MICROFRONTEND_ID) {
 
     enum class EventName {
         enable,disable
