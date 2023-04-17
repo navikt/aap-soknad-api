01c2d1e8bf4b50af8339b7a24e0c12db296d39c8
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-04-17 16:10:38 +0200

    log

--- src/main/kotlin/no/nav/aap/api/søknad/model/PDFKvittering.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/model/PDFKvittering.kt
@@ -1,18 +1,21 @@
 package no.nav.aap.api.søknad.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
-data class PDFKvittering(val temaer: List<Tema>, val mottattdato: LocalDateTime = LocalDateTime.now()) {
-    data class Tema(val type: String, val overskrift: String?, val underblokker: List<Blokk>) {
-        data class Blokk(val type: String,
-                         val overskrift: String?,
-                         val tabellrader: List<Blokk>?,
-                         val tekst: String?,
-                         val tittel: String?,
-                         val punkter: List<String>?,
-                         val felt: String?,
-                         val felter: List<Blokk>?,
-                         val indent: Boolean?,
-                         val verdi: String?)
+data class PDFKvittering(val temaer : List<Tema>, @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Europe/OSlo")
+val mottattdato : LocalDateTime = LocalDateTime.now()) {
+
+    data class Tema(val type : String, val overskrift : String?, val underblokker : List<Blokk>) {
+        data class Blokk(val type : String,
+                         val overskrift : String?,
+                         val tabellrader : List<Blokk>?,
+                         val tekst : String?,
+                         val tittel : String?,
+                         val punkter : List<String>?,
+                         val felt : String?,
+                         val felter : List<Blokk>?,
+                         val indent : Boolean?,
+                         val verdi : String?)
     }
 }
