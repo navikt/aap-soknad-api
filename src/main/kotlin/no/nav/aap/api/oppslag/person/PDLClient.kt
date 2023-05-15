c74159e01a18728e68a46c4b51b77a989c3b42ba
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-15 12:37:50 +0200

    retry på webclient nivå for pdl og saf

--- src/main/kotlin/no/nav/aap/api/oppslag/person/PDLClient.kt
+++ src/main/kotlin/no/nav/aap/api/oppslag/person/PDLClient.kt
@@ -1,6 +1,5 @@
 package no.nav.aap.api.oppslag.person
 
-import io.github.resilience4j.retry.annotation.Retry
 import io.micrometer.observation.annotation.Observed
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -13,13 +12,9 @@ class PDLClient(private val adapter : PDLWebClientAdapter) {
 
     private val log = LoggerFactory.getLogger(PDLClient::class.java)
 
-    @Retry(name =  "graphql")
     fun søkerUtenBarn() = adapter.søker()
 
-    @Retry(name =  "graphql")
     fun harBeskyttetBarn(barn : List<Barn>) = adapter.harBeskyttetBarn(barn)
-
-    @Retry(name =  "graphql")
     fun søkerMedBarn() = adapter.søker(true)
 
     override fun toString() = "${javaClass.simpleName} [pdl=$adapter]"
