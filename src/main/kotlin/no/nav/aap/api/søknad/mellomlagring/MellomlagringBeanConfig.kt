4868dcf4b9a5c21b303014bb4a86e9241996f006
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-19 23:27:35 +0200

    prøv med integration

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
@@ -10,12 +10,14 @@ import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.*
 import com.google.cloud.storage.NotificationInfo.*
 import com.google.cloud.storage.NotificationInfo.EventType.*
 import com.google.cloud.storage.StorageOptions
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
+import org.springframework.messaging.MessageChannel
 import org.threeten.bp.Duration
 import no.nav.aap.api.config.Metrikker
 import no.nav.aap.api.søknad.minside.MinSideClient
@@ -42,15 +44,18 @@ class MellomlagringBeanConfig {
         .service
 
     @Bean
-    fun storageChannelAdapter(cfg: BucketConfig, template : PubSubTemplate) =
+    @Qualifier(STORAGE_CHANNEL)
+    @Primary
+    fun storageInputChannel() = DirectChannel()
+    @Bean
+    fun storageChannelAdapter(cfg: BucketConfig, template : PubSubTemplate,  @Qualifier(STORAGE_CHANNEL) channel: MessageChannel) =
         PubSubInboundChannelAdapter(template, cfg.mellom.subscription.navn).apply {
-            outputChannel = DirectChannel()
+            outputChannel = channel
             ackMode = MANUAL
         }
 
     @Bean
     @ServiceActivator(inputChannel = STORAGE_CHANNEL)
-    @Primary
     fun messageReceiver( minside: MinSideClient, cfg: BucketConfig, mapper: ObjectMapper, metrikker: Metrikker) =
         StoragePubMessageHandler(minside,cfg,mapper,metrikker)
 
