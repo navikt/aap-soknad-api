65ee42a1374619c12f30567659206f577e6be74e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-19 23:11:27 +0200

    prøv med integration

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/StoragePubMessageHandler.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/StoragePubMessageHandler.kt
@@ -8,12 +8,18 @@ import org.springframework.messaging.MessageHandler
 import no.nav.aap.api.config.Metrikker
 import no.nav.aap.api.søknad.minside.MinSideClient
 import no.nav.aap.api.søknad.minside.PubSubMessageExtensions.handle
+import no.nav.aap.util.LoggerUtil
 
 class StoragePubMessageHandler(private val minside: MinSideClient, private val cfg: BucketConfig, private val mapper: ObjectMapper, private val  metrikker: Metrikker) :
     MessageHandler {
+
+    private val log = LoggerUtil.getLogger(javaClass)
+
     override fun handleMessage(m : Message<*>) {
+        log.trace("PubSub handling")
         m.headers.get(ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage::class.java)?.let {
             it.apply {
+                log.trace("PubSub acking ${it.pubsubMessage.messageId}")
                 ack()
                 pubsubMessage.handle(minside,cfg,mapper,metrikker)
             }
