70594d27f019f55b4ec16d5cc573f8fbb39f25b8
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-19 22:42:48 +0200

    prøv med integration

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
@@ -6,7 +6,6 @@ import com.google.cloud.ServiceOptions
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
 import com.google.cloud.spring.pubsub.integration.AckMode.MANUAL
 import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter
-import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
 import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.*
 import com.google.cloud.storage.NotificationInfo.*
 import com.google.cloud.storage.NotificationInfo.EventType.*
@@ -18,19 +17,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
-import org.springframework.messaging.Message
 import org.springframework.messaging.MessageChannel
-import org.springframework.messaging.MessageHandler
 import org.threeten.bp.Duration
 import no.nav.aap.api.config.Metrikker
 import no.nav.aap.api.søknad.minside.MinSideClient
-import no.nav.aap.api.søknad.minside.PubSubMessageExtensions.handle
-import no.nav.aap.util.LoggerUtil
 
 @Configuration(proxyBeanMethods = false)
 class MellomlagringBeanConfig {
-    private val log = LoggerUtil.getLogger(javaClass)
-
 
     @Bean
     @Primary
@@ -51,13 +44,13 @@ class MellomlagringBeanConfig {
         .service
 
     @Bean
-    @Qualifier("storageInputChannel")
+    @Qualifier(STORAGE_CHANNEL)
     fun pubsubInputChannel() = DirectChannel()
 
     @Bean
     fun storageChannelAdapter(cfg: BucketConfig, @Qualifier(STORAGE_CHANNEL) channel : MessageChannel, template : PubSubTemplate) =
         PubSubInboundChannelAdapter(template, cfg.mellom.subscription.navn).apply {
-            setOutputChannel(channel)
+            outputChannel = channel
             ackMode = MANUAL
         }
 
@@ -65,20 +58,6 @@ class MellomlagringBeanConfig {
     @ServiceActivator(inputChannel = STORAGE_CHANNEL)
     fun messageReceiver( minside: MinSideClient, cfg: BucketConfig, mapper: ObjectMapper, metrikker: Metrikker) =
         StoragePubMessageHandler(minside,cfg,mapper,metrikker)
-
-
-    class StoragePubMessageHandler(private val minside: MinSideClient, private val cfg: BucketConfig, private val mapper: ObjectMapper, private val  metrikker: Metrikker) :
-        MessageHandler {
-        override fun handleMessage(m : Message<*>) {
-            m.headers.get(ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage::class.java)?.let {
-                it.apply {
-                    ack()
-                    pubsubMessage.handle(minside,cfg,mapper,metrikker)
-                }
-            }
-        }
-    }
-
 
     companion object  {
         private const val STORAGE_CHANNEL = "storageInputChannel"
---
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/StoragePubMessageHandler.kt
@@ -0,0 +1,22 @@
+package no.nav.aap.api.søknad.mellomlagring
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHandler
+import no.nav.aap.api.config.Metrikker
+import no.nav.aap.api.søknad.minside.MinSideClient
+import no.nav.aap.api.søknad.minside.PubSubMessageExtensions.handle
+
+class StoragePubMessageHandler(private val minside: MinSideClient, private val cfg: BucketConfig, private val mapper: ObjectMapper, private val  metrikker: Metrikker) :
+    MessageHandler {
+    override fun handleMessage(m : Message<*>) {
+        m.headers.get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage::class.java)?.let {
+            it.apply {
+                ack()
+                pubsubMessage.handle(minside,cfg,mapper,metrikker)
+            }
+        }
+    }
+}
