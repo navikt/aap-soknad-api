0240a9ba5a449c4462df9c2338ab5bcccccd541c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-22 21:04:26 +0200

    chore

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
@@ -9,6 +9,7 @@ import com.google.pubsub.v1.PubsubMessage
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageHandler
 import no.nav.aap.api.søknad.mellomlagring.BucketConfig.MellomlagringBucketConfig
+import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangsOpprettelse
@@ -21,44 +22,45 @@ class MellomlagringEventSubscriber(private val minside: MinSideClient, private v
 
     private val log = LoggerUtil.getLogger(javaClass)
 
-    override fun handleMessage(m : Message<*>) {
+    override fun handleMessage(m : Message<out Any>) {
         m.headers.get(ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage::class.java)?.let {
-            it.apply {
-                handle(pubsubMessage)
-            }
+            handle(it.pubsubMessage)
         }
     }
     private fun handle(msg : PubsubMessage) =
-        with(msg) {
-            val eventType = eventType()
-            metadata(mapper)?.let { md ->
-                log.trace("Event type {} med metadata {}", eventType, md)
-                with(md) {
-                    when (eventType) {
-                        OBJECT_FINALIZE -> if (førstegangsOpprettelse()) {
-                            minside.opprettUtkast(fnr, "Du har en påbegynt $tittel", type, eventId)
-                        }
-                        else {
-                            minside.oppdaterUtkast(fnr, "Du har en påbegynt $tittel", type)
-                        }
+        msg.metadata(mapper)?.let {
+            log.trace("Event type {} med metadata {}", msg.eventType(), it)
+            when (msg.eventType()) {
+                OBJECT_FINALIZE -> oppdatert(msg, it)
+                OBJECT_DELETE -> slettet(msg, it)
+                else -> log.warn("Event ${msg.eventType()} ikke håndtert (dette skal aldri skje)")
+            }
+        } ?: log.warn("Fant ikke forventede metadata i event $this ${msg.attributesMap}")
 
-                        OBJECT_DELETE -> if (endeligSlettet()) {
-                            md.varighet()?.let {
-                                log.info("Endelig slettet etter ${it.toSeconds()}s")
-                                if (it > cfg.varighet) {
-                                    log.info("Slettet endelig mellomlagring etter ${cfg.varighet.toDays()} dager for $md")
-                                }
-                            }
-                            minside.avsluttUtkast(fnr, type)
-                        }
-                        else {
-                            Unit.also {
-                                log.trace("Slettet grunnet ny versjon, ingen oppdatering av utkast for {}", fnr)
-                            }
-                        }
-                        else -> log.warn("Event $eventType ikke håndtert (dette skal aldri skje)")
-                    }
+
+    private fun slettet(msg : PubsubMessage,  md : Metadata) =
+        if (msg.endeligSlettet()) {
+            md.varighet()?.let {
+                log.info("Endelig slettet etter ${it.toSeconds()}s")
+                if (it > cfg.varighet) {
+                    log.info("Slettet endelig mellomlagring etter ${cfg.varighet.toDays()} dager for $md")
                 }
-            } ?: log.warn("Fant ikke forventede metadata i event $this $attributesMap")
+            }
+            minside.avsluttUtkast(md.fnr, md.type)
+        }
+        else {
+            Unit.also {
+                log.trace("Slettet grunnet ny versjon, ingen oppdatering av utkast for {}", md.fnr)
+            }
+        }
+
+    private fun oppdatert(msg : PubsubMessage, md : Metadata)  =
+        with(md) {
+            if (msg.førstegangsOpprettelse()) {
+                minside.opprettUtkast(fnr, "Du har en påbegynt $tittel", type, eventId)
+            }
+            else {
+                minside.oppdaterUtkast(fnr, "Du har en påbegynt $tittel", type)
+            }
         }
 }
