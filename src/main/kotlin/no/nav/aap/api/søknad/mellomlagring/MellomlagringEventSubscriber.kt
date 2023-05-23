8f1d83da4eb999a5197cd20f7b4f8b3f0893f43c
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-23 14:31:59 +0200

    kotlin dsl

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringEventSubscriber.kt
@@ -6,10 +6,9 @@ import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.ORIGINAL_MESSAGE
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_DELETE
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_FINALIZE
 import com.google.pubsub.v1.PubsubMessage
-import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHandler
 import org.springframework.stereotype.Component
-import no.nav.aap.api.søknad.mellomlagring.MellomlagringBeanConfig.Companion.STORAGE_CHANNEL
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
@@ -20,16 +19,16 @@ import no.nav.aap.util.LoggerUtil
 
 
 @Component
-class MellomlagringEventSubscriber(private val minside: MinSideClient, private val cfg: BucketConfig, private val mapper: ObjectMapper) { //: MessageHandler {
+class MellomlagringEventSubscriber(private val minside: MinSideClient, private val cfg: BucketConfig, private val mapper: ObjectMapper) : MessageHandler {
 
     private val log = LoggerUtil.getLogger(javaClass)
 
-     fun handleMessage(m : Message<out Any>) {
+     override fun handleMessage(m : Message<out Any>) {
         m.headers.get(ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage::class.java)?.let {
-           // handle(it.pubsubMessage)
+            handle(it)
         }
     }
-    @ServiceActivator(inputChannel = STORAGE_CHANNEL)
+  //  @ServiceActivator(inputChannel = STORAGE_CHANNEL)
     private fun handle(msg : BasicAcknowledgeablePubsubMessage) =
         msg.pubsubMessage.metadata(mapper)?.let {md ->
             val eventType = msg.pubsubMessage.eventType().also {
