49f6f5180bce5ab1214056a3a9f0ac833211a900
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-23 13:40:03 +0200

    kotlin dsl

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagringBeanConfig.kt
@@ -54,7 +54,7 @@ class MellomlagringBeanConfig {
     fun gcpStorageInputChannel() = DirectChannel()
 
     @Bean
-    fun gcpStorageFlow(@Qualifier(STORAGE_CHANNEL) channel: MessageChannel, eventHandler: MellomlagringEventSubscriber) =
+    fun gcpStorageFlow(@Qualifier(STORAGE_CHANNEL) channel: MessageChannel, eventHandler: MellomlagringEventSubscriber, transformer: TestTransformer) =
         integrationFlow {
             channel(channel)
             wireTap {
@@ -62,10 +62,11 @@ class MellomlagringBeanConfig {
                     log.trace("Headers: {}", it.headers)
                 }
             }
-            transform(testTransformer())
+            transform(transformer)
             handle(eventHandler)
         }
 
+   @Bean
     fun testTransformer() = TestTransformer()
 
     class TestTransformer {
