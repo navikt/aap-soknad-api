629088d4a145dbe73e4f3f137a8a735d9a5bdf76
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-24 13:13:41 +0200

    del opp ny og gammel

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
@@ -1,8 +1,8 @@
 package no.nav.aap.api.søknad.mellomlagring
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
-import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.*
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_DELETE
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_FINALIZE
 import org.springframework.integration.annotation.Transformer
@@ -18,22 +18,22 @@ import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangs
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.metadata
 import no.nav.aap.util.LoggerUtil
 
-class GCPBucketEventTransformer {
+class GCPBucketEventTransformer(private val mapper: ObjectMapper) {
 
     private val log = LoggerUtil.getLogger(javaClass)
 
     @Transformer
-    fun payload(@Header(GcpPubSubHeaders.ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage?)  =
+    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage)  =
         try {
-            msg?.pubsubMessage?.let {
-                val md = it.metadata(jacksonObjectMapper())
+            msg.pubsubMessage.let {
+                val md = it.metadata(mapper)
                 log.trace("Metadata er {}", md)
                 when ( it.eventType()) {
                     OBJECT_FINALIZE -> if (it.førstegangsOpprettelse()) MellomlagringsHendelse(OPPRETTET,md) else MellomlagringsHendelse(OPPDATERING,md)
                     OBJECT_DELETE -> if (it.endeligSlettet()) MellomlagringsHendelse(ENDELIG_SLETTING,md) else MellomlagringsHendelse(IGNORER,md)
-                    else -> MellomlagringsHendelse(IGNORER,md)
+                    else -> MellomlagringsHendelse(IGNORER)
                 }
-            } ?: MellomlagringsHendelse(IGNORER)
+            }
         } catch (e: Exception) {
             MellomlagringsHendelse(IGNORER)
         }
