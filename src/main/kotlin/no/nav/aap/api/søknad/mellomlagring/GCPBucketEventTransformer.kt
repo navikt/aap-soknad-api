e2ed60cdf72fbdafd1779b4ef37c28db0e46151e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-25 10:01:56 +0200

    event type fra heder?

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
@@ -14,7 +14,6 @@ import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventTy
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventType.OPPRETTET
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
-import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangsOpprettelse
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.metadata
 import no.nav.aap.util.LoggerUtil
@@ -29,13 +28,14 @@ class GCPBucketEventTransformer(private val mapper: ObjectMapper) {
             msg.pubsubMessage.let {
                 val md = it.metadata(mapper)
                 log.trace("Metadata er {}, et er $et", md)
-                when ( it.eventType()) {
+                when (et) {
                     OBJECT_FINALIZE -> if (it.førstegangsOpprettelse()) MellomlagringsHendelse(OPPRETTET,md) else MellomlagringsHendelse(OPPDATERT,md)
                     OBJECT_DELETE -> if (it.endeligSlettet()) MellomlagringsHendelse(ENDELIG_SLETTET,md) else MellomlagringsHendelse(IGNORERT,md)
                     else -> MellomlagringsHendelse(IGNORERT)
                 }
             }
         } catch (e: Exception) {
+            log.warn("OOPS",e)
             MellomlagringsHendelse(IGNORERT)
         }
 
