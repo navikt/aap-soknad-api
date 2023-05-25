34a2a64b6a1f92e3a417a26db0dc1d0cb4ff49b0
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-25 10:51:26 +0200

    event type fra header?

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
@@ -3,8 +3,10 @@ package no.nav.aap.api.søknad.mellomlagring
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
 import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.*
+import com.google.cloud.storage.NotificationInfo.EventType
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_DELETE
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_FINALIZE
+import com.nimbusds.openid.connect.sdk.claims.LogoutTokenClaimsSet.EVENT_TYPE
 import org.springframework.integration.annotation.Transformer
 import org.springframework.messaging.handler.annotation.Header
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventType.ENDELIG_SLETTET
@@ -13,7 +15,6 @@ import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventTy
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventType.OPPRETTET
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
-import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangsOpprettelse
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.metadata
 import no.nav.aap.util.LoggerUtil
@@ -23,11 +24,11 @@ class GCPBucketEventTransformer(private val mapper: ObjectMapper) {
     private val log = LoggerUtil.getLogger(javaClass)
 
     @Transformer
-    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage)  =
+    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage, @Header(EVENT_TYPE) eventType : EventType)  =
              msg.pubsubMessage.let {
                  val md = it.metadata(mapper)
                  log.trace("Metadata er {}", md)
-                 when (it.eventType()) {
+                 when (eventType) {
                      OBJECT_FINALIZE -> if (it.førstegangsOpprettelse()) MellomlagringsHendelse(OPPRETTET, md) else MellomlagringsHendelse(OPPDATERT, md)
                      OBJECT_DELETE -> if (it.endeligSlettet()) MellomlagringsHendelse(ENDELIG_SLETTET, md) else MellomlagringsHendelse(IGNORERT, md)
                      else -> MellomlagringsHendelse(IGNORERT)
