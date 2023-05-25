7694e414de57ffd7f8ed1024d5cf038cbb2ce1d5
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-25 10:21:16 +0200

    event type fra heder?

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
@@ -3,7 +3,6 @@ package no.nav.aap.api.søknad.mellomlagring
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
 import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders.*
-import com.google.cloud.storage.NotificationInfo.EventType
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_DELETE
 import com.google.cloud.storage.NotificationInfo.EventType.OBJECT_FINALIZE
 import org.springframework.integration.annotation.Transformer
@@ -14,6 +13,7 @@ import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventTy
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventType.OPPRETTET
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
+import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangsOpprettelse
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.metadata
 import no.nav.aap.util.LoggerUtil
@@ -23,19 +23,19 @@ class GCPBucketEventTransformer(private val mapper: ObjectMapper) {
     private val log = LoggerUtil.getLogger(javaClass)
 
     @Transformer
-    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage,@Header("eventType") et: EventType)  =
+    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage)  =
         try {
             msg.pubsubMessage.let {
                 val md = it.metadata(mapper)
-                log.trace("Metadata er {}, et er $et", md)
-                when (et) {
+                log.trace("Metadata er {}", md)
+                when ( it.eventType()) {
                     OBJECT_FINALIZE -> if (it.førstegangsOpprettelse()) MellomlagringsHendelse(OPPRETTET,md) else MellomlagringsHendelse(OPPDATERT,md)
                     OBJECT_DELETE -> if (it.endeligSlettet()) MellomlagringsHendelse(ENDELIG_SLETTET,md) else MellomlagringsHendelse(IGNORERT,md)
                     else -> MellomlagringsHendelse(IGNORERT)
                 }
             }
         } catch (e: Exception) {
-            log.warn("OOPS",e)
+            log.warn("OOPS")
             MellomlagringsHendelse(IGNORERT)
         }
 
