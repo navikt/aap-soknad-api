0cb696b179772649475fd28565c70dcba06c7870
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-25 14:19:26 +0200

    event type fra header?

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/GCPBucketEventTransformer.kt
@@ -15,6 +15,7 @@ import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventTy
 import no.nav.aap.api.søknad.mellomlagring.GCPBucketEventTransformer.GCPEventType.OPPRETTET
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.Metadata
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.endeligSlettet
+import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.eventType
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.førstegangsOpprettelse
 import no.nav.aap.api.søknad.mellomlagring.PubSubMessageExtensions.metadata
 import no.nav.aap.util.LoggerUtil
@@ -24,11 +25,11 @@ class GCPBucketEventTransformer(private val mapper: ObjectMapper) {
     private val log = LoggerUtil.getLogger(javaClass)
 
     @Transformer
-    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage, @Header(EVENT_TYPE) eventType : EventType)  =
+    fun payload(@Header(ORIGINAL_MESSAGE) msg : BasicAcknowledgeablePubsubMessage)  =
              msg.pubsubMessage.let {
                  val md = it.metadata(mapper)
                  log.trace("Metadata er {}", md)
-                 when (eventType) {
+                 when (it.eventType()) {
                      OBJECT_FINALIZE -> if (it.førstegangsOpprettelse()) MellomlagringsHendelse(OPPRETTET, md) else MellomlagringsHendelse(OPPDATERT, md)
                      OBJECT_DELETE -> if (it.endeligSlettet()) MellomlagringsHendelse(ENDELIG_SLETTET, md) else MellomlagringsHendelse(IGNORERT, md)
                      else -> MellomlagringsHendelse(IGNORERT)
