a249644cb9f227284098283299817757b40472b0
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-05-30 21:47:57 +0200

    dabble with coroutines

--- src/main/kotlin/no/nav/aap/api/config/GlobalBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/api/config/GlobalBeanConfig.kt
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.nimbusds.jwt.JWTClaimNames.JWT_ID
-import io.micrometer.observation.Observation.Context
-import io.micrometer.observation.ObservationHandler
 import io.micrometer.observation.ObservationRegistry
 import io.micrometer.observation.ObservationTextPublisher
 import io.micrometer.observation.aop.ObservedAspect
@@ -15,6 +13,7 @@ import io.netty.handler.logging.LogLevel.TRACE
 import io.netty.handler.timeout.ReadTimeoutHandler
 import io.netty.handler.timeout.TimeoutException
 import io.netty.handler.timeout.WriteTimeoutHandler
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
@@ -92,6 +91,9 @@ import no.nav.security.token.support.core.context.TokenValidationContextHolder
 class GlobalBeanConfig(@Value("\${spring.application.name}") private val applicationName : String) {
 
     val log = getLogger(javaClass)
+
+    @Bean
+    fun grpcSpanExporter() = OtlpGrpcSpanExporter.builder().setEndpoint("tempo-distributor.nais-system:4317")
 
     @Bean
     fun observationTextPublisher() = ObservationTextPublisher(log::info)
