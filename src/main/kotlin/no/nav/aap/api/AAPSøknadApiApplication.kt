7f89fe2bd600da8f04b37bebfb4739f2f39f2912
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-06-07 12:41:42 +0200

    warn logs only

--- src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
+++ src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
@@ -1,5 +1,7 @@
 package no.nav.aap.api
 
+import io.micrometer.context.ThreadLocalAccessor
+import io.micrometer.context.ContextRegistry.getInstance    
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
@@ -10,6 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder.getRequestAttributes
+import org.springframework.web.context.request.RequestContextHolder.resetRequestAttributes
+import org.springframework.web.context.request.RequestContextHolder.setRequestAttributes
 import reactor.core.publisher.Hooks
 import no.nav.boot.conditionals.Cluster.Companion.profiler
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
@@ -30,7 +36,20 @@ class AAPSøknadApiApplication
         runApplication<AAPSøknadApiApplication>(*args) {
             setAdditionalProfiles(*profiler())
             Hooks.enableAutomaticContextPropagation()
-           // AccessorUtil.init()
+            getInstance().apply {
+                registerThreadLocalAccessor(RequestAttributesAccessor())
+            }
             applicationStartup = BufferingApplicationStartup(4096)
         }
     }
+
+private class RequestAttributesAccessor : ThreadLocalAccessor<RequestAttributes> {
+
+    override fun key() = RequestAttributesAccessor::class.java.name
+
+    override fun getValue() = getRequestAttributes()
+
+    override fun setValue(value : RequestAttributes) = setRequestAttributes(value)
+
+    override fun reset() = resetRequestAttributes()
+}
