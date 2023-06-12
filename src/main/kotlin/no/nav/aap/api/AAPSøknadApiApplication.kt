86b27669dc6824fa789b99454794cea1b88480ab
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-06-12 15:39:49 +0200

    observation predicate

--- src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
+++ src/main/kotlin/no/nav/aap/api/AAPSøknadApiApplication.kt
@@ -1,5 +1,7 @@
 package no.nav.aap.api
 
+import io.micrometer.context.ContextRegistry
+import io.micrometer.context.ThreadLocalAccessor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
@@ -10,7 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableScheduling
-import no.nav.aap.util.AccessorUtil
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.RequestContextHolder
+import reactor.core.publisher.Hooks
 import no.nav.boot.conditionals.Cluster.Companion.profiler
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
@@ -27,9 +31,25 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 class AAPSøknadApiApplication
 
     fun main(args: Array<String>) {
+        init()
         runApplication<AAPSøknadApiApplication>(*args) {
             setAdditionalProfiles(*profiler())
-            AccessorUtil.init()
             applicationStartup = BufferingApplicationStartup(4096)
         }
     }
+fun init() {
+    Hooks.enableAutomaticContextPropagation()
+    ContextRegistry.getInstance().apply {
+        registerThreadLocalAccessor(RequestAttributesAccessor())
+    }
+}
+class RequestAttributesAccessor : ThreadLocalAccessor<RequestAttributes> {
+
+    override fun key() = RequestAttributesAccessor::class.java.name
+
+    override fun getValue() = RequestContextHolder.getRequestAttributes()
+
+    override fun setValue(value : RequestAttributes) = RequestContextHolder.setRequestAttributes(value)
+
+    override fun reset() = RequestContextHolder.resetRequestAttributes()
+}
