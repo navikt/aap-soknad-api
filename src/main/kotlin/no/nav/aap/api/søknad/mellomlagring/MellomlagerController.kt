edff84005152fe49719fac7134ca97765528bea3
Anders Østby <anders.ostby@nav.no>
2023-07-07 10:45:21 +0200

    Returnerer 204 hvis respons fra mellomlager er null
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagerController.kt
+++ src/main/kotlin/no/nav/aap/api/søknad/mellomlagring/MellomlagerController.kt
@@ -1,30 +1,29 @@
 package no.nav.aap.api.søknad.mellomlagring
 
-import org.springframework.http.HttpStatus.CREATED
-import org.springframework.http.HttpStatus.NO_CONTENT
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import no.nav.aap.api.felles.SkjemaType
 import no.nav.aap.api.søknad.mellomlagring.dokument.Dokumentlager
 import no.nav.aap.util.Constants.IDPORTEN
 import no.nav.security.token.support.spring.ProtectedRestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @ProtectedRestController(value = ["buckets"], issuer = IDPORTEN)
-internal class MellomlagerController(private val mellomlager: Mellomlager, private val dokumentlager: Dokumentlager ) {
+internal class MellomlagerController(private val mellomlager: Mellomlager, private val dokumentlager: Dokumentlager) {
 
     @PostMapping("/lagre/{type}")
-    @ResponseStatus(CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     fun lagre(@PathVariable type: SkjemaType, @RequestBody data: String) = mellomlager.lagre(data, type)
 
     @GetMapping("/les/{type}")
-    fun les(@PathVariable type: SkjemaType) = mellomlager.les(type)
+    fun les(@PathVariable type: SkjemaType): ResponseEntity<String> {
+        val les = mellomlager.les(type)
+        if (les == null) return ResponseEntity<String>(HttpStatus.NO_CONTENT)
+        return ResponseEntity(les, HttpStatus.OK)
+    }
 
     @DeleteMapping("/slett/{type}")
-    @ResponseStatus(NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun slett(@PathVariable type: SkjemaType) = mellomlager.slett(type).also { dokumentlager.slettAlleDokumenter() }
 
 }
